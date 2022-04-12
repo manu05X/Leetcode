3afc99393112042c3da3f8d33d54a1c49e990088
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2022-04-12 08:32:17 +0530

    Time: 144 ms (65.64%), Space: 28.8 MB (90.37%) - LeetHub

--- 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
+++ 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int minSteps(string s, string t) {
-    int ans = 0;
-    vector<int> sCount(26, 0), tCount(26, 0);
-	
-    for(char ch: s){
-		sCount[ch - 'a']++;
-	}
-    for(char ch: t) {
-		tCount[ch - 'a']++;
-	}
-    for (int i = 0; i < 26; i++) 
-		ans += abs(sCount[i] - tCount[i]);
+        vector<int> freqMap(26,0);
 		
-    return ans;
-}
+        for(auto ch: s)
+			freqMap[ch-'a']++;
+        for(auto ch: t)
+			freqMap[ch-'a']--;
+			
+        int ans=0;
+        for(int i=0;i<26;i++)
+			ans+=abs(0-freqMap[i]);
+			
+        return ans;
+    }
 };
