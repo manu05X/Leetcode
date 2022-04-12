486afb7579ec3ae1333d5445c54c40ce98516f83
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2022-04-12 08:35:34 +0530

    Time: 258 ms (87.51%), Space: 7.9 MB (91.40%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,44 +1,40 @@
 class Solution {
-  	public:
-  		bool search(vector<vector<char>>& board, int i, int j, string& word, int idx, vector<vector<bool>>& visited){
-  			// if it is the last char of word, Signifies that we have reached the end of word
-  			if(idx == word.length()-1)
-  				return true;
-  			// else mark visited and call DFS for neighbour in 4 direction
-  			visited[i][j] = true;
-  			// call DFS for neighbour in 4 direction and increase the index
-  			if(i > 0 && !visited[i-1][j] && board[i-1][j] == word[idx+1] && search(board, i-1,j,word,idx+1,visited))
-  				return true;
+  public:
 
-  			if(j > 0 && !visited[i][j-1] && board[i][j-1] == word[idx+1] && search(board, i,j-1,word,idx+1,visited))
-  				return true;
+  	bool dfs(vector<vector<char>>&board, int count, int i, int j, string& word)
+  	{
+  		if(word.size() == count) //Signifies that we have reached the end of search
+  			return true;
 
-  			if(i < board.size()-1 && !visited[i+1][j] && board[i+1][j] == word[idx+1] && search(board, i+1,j,word,idx+1,visited))
-  				return true;
+  		if(i<0 || j<0 || i>=board.size() || j>=board[0].size() || board[i][j]!=word[count])
+  		return false;
+  		//We check if element is within bounds and then check if the character at that is the same as the corresponding character in string word
 
-  			if(j < board[0].size()-1 && !visited[i][j+1] && board[i][j+1] == word[idx+1] && search(board, i,j+1,word,idx+1,visited))
-  				return true;
-  			// if backtrack mark it as false
-  			visited[i][j] = false;
-  			// if none of the case satisfy then return it as false;
-  			return false;
+
+  		char temp = board[i][j];
+  		board[i][j] = ' '; //To show that we have visited this node
+
+  		bool res = dfs(board, count+1, i+1, j, word) || dfs(board, count+1, i-1, j, word) || dfs(board, count+1, i, j+1, word) ||dfs(board, count+1, i, j-1, word); //DFS in all 4 directions
+
+  		board[i][j] = temp; //Restore the element after checking
+
+  		return res;
   	}
+  	
+  	bool exist(vector<vector<char>>& board, string word)
+  	{
+  		if(board.empty())
+  			return false;
 
-  	bool exist(vector<vector<char>>& board, string word) {
-  		int n = board.size();
-  		int m = board[0].size();
-
-  		//initilizing visited with false
-  		vector<vector<bool>> visited(n, vector<bool>(m,false));
-
-  		for(int i = 0; i < n; i++)
+  		for(int i=0; i<board.size(); i++)
   		{
-  			for(int j = 0; j < m; j++)
-  			{   //if 0th char  matches then search for rest of char against that index in dfs search
-  				if(board[i][j] == word[0] && search(board, i,j,word,0,visited))
+  			for(int j=0; j<board[0].size(); j++)
+  			{
+  				if(dfs(board, 0, i, j, word))
   					return true;
   			}
   		}
+
   		return false;
-  	 }
-  	};
+  	}
+  };
