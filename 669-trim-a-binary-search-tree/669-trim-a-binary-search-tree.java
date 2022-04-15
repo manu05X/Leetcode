3c103f73b25c32067fc8c88b4dfc1b116a95d5fe
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2022-04-15 07:55:14 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (87.65%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.java
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.java
@@ -14,18 +14,26 @@
  * }
  */
 class Solution {
-    public TreeNode trimBST(TreeNode root, int low, int high) {
-        if(root == null)
-            return root;
-        
-        if(root.val > high)
-            return trimBST(root.left, low, high);
-        if(root.val < low)
-            return trimBST(root.right, low, high);
-        
-        root.left = trimBST(root.left, low, high);
-        root.right = trimBST(root.right, low, high);
-        
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        if (root == null) 
+            return null;
+
+        // If the value of this node is less than L, then the whole left subtree will be smaller, 
+        // so we can discard the left sub tree and return the root of the trimmed right sub tree 
+        if (root.val < L) 
+            return trimBST(root.right, L, R);
+
+        // If the value of this node is greater than R, then the whole right subtree will be greater
+        // so we can discard the right sub tree and return the root of the trimmed left sub tree
+        if (root.val > R) 
+            return trimBST(root.left, L, R);
+
+        // If the value of this node does not need to be deleted, 
+        // we need to pass this recursive call downwards to both sides
+        root.left = trimBST(root.left, L, R);
+        root.right = trimBST(root.right, L, R);
+
+        // All the processing of the subtrees done, now return this node
         return root;
     }
 }
