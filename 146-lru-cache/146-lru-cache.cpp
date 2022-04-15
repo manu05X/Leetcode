3ed98374e109875357a6e5d6de03ad6f551467f9
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2022-04-15 08:48:01 +0530

    Update 146-lru-cache.cpp

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -48,3 +48,97 @@ public:
  * int param_1 = obj->get(key);
  * obj->put(key,value);
  */
+// Method 2
+
+typedef struct Entry_t {
+	int address;
+	int value;
+	Entry_t* next;
+	Entry_t* prev;
+	Entry_t(int _add, int _val, Entry_t *_next, Entry_t* _prev) :
+			address(_add), value(_val), next(_next), prev(_prev) {
+	}
+
+} Entry;
+
+class LRUCache {
+	int size;
+	Entry* head;
+	Entry* tail;
+	unordered_map<int, Entry*> map;
+	int len;
+
+	void evict() { //Depending on policy
+		//remove last one
+		if (!tail)
+			return;
+		if (tail->prev)
+			tail->prev->next = nullptr;
+		else
+			//single Entry
+			head = nullptr;
+		Entry* temp = tail;
+		tail = tail->prev;
+		//remove from map
+		map.erase(temp->address);
+		delete temp;
+		len--;
+	}
+public:
+    LRUCache(int capacity) :
+			size(capacity), head(nullptr), tail(nullptr), len(0) {
+	}
+    
+    int get(int key) {
+        auto it = map.find(key);
+		if (it == map.end())
+			return -1;
+		//bring it to front
+		Entry* ptr = it->second;
+		if (ptr == head)		// nothing to do
+			return ptr->value;
+		if (ptr->prev)
+			ptr->prev->next = ptr->next;
+		if (ptr->next)
+			ptr->next->prev = ptr->prev;
+		else
+			tail = ptr->prev;
+
+		//Now put it at the front
+		ptr->next = head;
+		if (head)
+			head->prev = ptr;
+		head = ptr;
+		return ptr->value;
+    }
+    
+    void put(int key, int value) {
+        auto it = map.find(key);
+		if (it!=map.end()){
+			it->second->value = value;
+			get(key);
+			return;
+		}
+		//New Entry
+		if (len == size)		//full
+			evict();
+		//Add
+		Entry* ptr = new Entry(key, value, nullptr, nullptr);
+		ptr->next = head;
+		if (head)
+			head->prev = ptr;
+		head = ptr;
+		if (!tail)
+			tail = ptr;
+		//Add it to the map
+		map.insert(make_pair(key, ptr));
+		len++;
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
