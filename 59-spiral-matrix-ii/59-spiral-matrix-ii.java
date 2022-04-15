2a5b5c93fc505f125b993db968d0e1027235ac42
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2022-04-15 18:56:52 +0530

    Update 59-spiral-matrix-ii.java

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.java
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.java
@@ -20,3 +20,38 @@ class Solution {
         return result;
     }
 }
+/*<-------------------------------------METHOD 2--------------------------------------------------------------------------------->*/
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int r1 = 0, r2 = n-1;
+        int c1 = 0, c2 = n-1;
+        int cnt = 0;
+        
+        int[][] res = new int[n][n];
+        
+        while(r1 <= r2 && c1 <= c2)
+        {
+            for(int i = c1; i <= c2; i++){
+                res[r1][i] = ++cnt;
+            }
+            
+            for(int i = r1+1; i<=r2; i++){
+                res[i][c2] = ++cnt;
+            }
+            
+            for(int i = c2-1; i>= c1; i--){
+                res[r2][i] = ++cnt;
+            }
+            for(int i = r2-1; i > r1; i--){
+                res[i][c1] = ++cnt;
+            }
+            
+            r1++;
+            c1++;
+            r2--;
+            c2--;
+            
+        }
+        return res;
+    }
+}
