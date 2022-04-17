8082bb5f5d493367921ad93acf12c92e4baa8730
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2022-04-17 07:57:15 +0530

    Update 897-increasing-order-search-tree.java

--- 897-increasing-order-search-tree/897-increasing-order-search-tree.java
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.java
@@ -33,3 +33,43 @@ class Solution {
     }
     
 }
+/*<---------------------------------------------------------------------------------------------------------------------->*/
+// Itration Solution
+
+class Solution {
+    public TreeNode increasingBST(TreeNode root) {
+        if (root == null) 
+            return root;
+        Deque<TreeNode> dq = new ArrayDeque<>();
+        TreeNode cur = root;
+        TreeNode prev = null;
+        TreeNode newRoot = null;
+        
+        while (!dq.isEmpty() || cur != null) 
+        {
+            //keep going left, until you hit a null
+            while (cur != null) {
+                dq.offer(cur);
+                cur = cur.left;
+            }
+            
+            // pop from the stack and create the new root if not already created.
+            cur = dq.pollLast();
+            if (newRoot == null) 
+            {
+                newRoot = cur;
+            }
+            
+            //assign the current element to previous element's right.
+            if (prev != null) 
+            {
+                prev.right = cur;
+                cur.left = null;
+            }
+            //point prev to cur and move cur to right.
+            prev = cur;
+            cur = cur.right;
+        }
+        return newRoot;
+    }
+}
