0c124b0863c64a36b629812458ab4dedf2572b47
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2022-04-17 17:29:19 +0530

    Time: 237 ms (66.67%), Space: 103.7 MB (33.33%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
     int minimumRounds(vector<int>& tasks) {
-        unordered_map<int,int> mp;
-        
-        for(int i = 0; i < tasks.size(); i++){
-            mp[tasks[i]]++;
-        }
-        
-        int ans = 0;
-        for(auto i : mp){
-            // cout << "First : " <<i.first <<" Second : "<<i.second <<"\n";
-            int freq = i.second;
-            
-            if(freq == 1)
+        unordered_map<int, int> m; // map to store task and its frequency
+        for(int i=0; i<tasks.size(); i++)
+            m[tasks[i]]++; // storing frequencies in map
+        int answer=0;
+        for(auto x:m){
+            if(x.second==1) // if a task is present only once, return -1
                 return -1;
-            if(freq % 3 == 0)
-            {
-                ans += freq/3;
-                // cout <<freq <<" freq % 3 == 0 , "<< ans <<endl;
-            }
-            else
-            {
-                ans += freq/3 + 1;
-                // cout << ans <<endl;
-            }
+            answer+=(x.second)/3;
+            /* if x.second is not divisible by 3, then we would be required to consider the cases when only two tasks are selected 
+            - in last round for (x.second%3==2)
+            - in last two rounds (x.second%3==1) */
+            if(x.second%3==1 || x.second%3==2)
+                answer++;
         }
-        
-        return ans;
+        return answer;
     }
 };
