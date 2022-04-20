54766869a1de243cca69b4b980ad03543588d8ed
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2022-04-20 08:48:06 +0530

    Update 1710-maximum-units-on-a-truck.cpp

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+        sort(boxTypes.begin(), boxTypes.end(), Comparator());
+        int unitCount = 0;
+        for (auto boxType : boxTypes) {
+            int boxCount = min(truckSize, boxType[0]);
+            unitCount += boxCount * boxType[1];
+            truckSize -= boxCount;
+            if (truckSize == 0) break;
+        }
+        return unitCount;
+    }
+
+    struct Comparator {
+        bool operator()(vector<int> const& p1, vector<int> const& p2) {
+            return p1[1] > p2[1];
+        }
+    };
+};
+
+/*<------------------------------------Using Priority Queue---------------------------------------------------------->*/
+
+class Solution {
+public:
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
         priority_queue<vector<int>, vector<vector<int>>, Comparator> queue;
         for (auto boxType : boxTypes) {
             queue.push(boxType);
