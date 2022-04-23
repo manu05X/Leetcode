07832dd00218a3e0403de663e45a8291d7dba6be
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2022-04-23 10:09:29 +0530

    Time: 3 ms (28.35%), Space: 6 MB (56.18%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+       if(n <= 2)
+            return n;
+        int mem[n+1];
+        mem[1] = 1;
+        mem[2] = 2;
+        
+        for(int i = 3; i <= n; i++)
+        {
+            mem[i] = mem[i-1] + mem[i-2];
+        }
+        return mem[n]; 
+    }
+};
