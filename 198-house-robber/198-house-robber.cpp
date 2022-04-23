24788fb8f87e09435b4c7ddcf90a40da1a3d2492
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2022-04-23 16:21:16 +0530

    Time: 3 ms (50.14%), Space: 7.7 MB (48.13%) - LeetHub

---
+++ 198-house-robber/198-house-robber.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int N = nums.size();
+        
+        // Special handling for empty array case.
+        if (N == 0) {
+            return 0;
+        }
+        
+        int robNext, robNextPlusOne;
+        
+        // Base case initializations.
+        robNextPlusOne = 0;
+        robNext= nums[N - 1];
+        
+        // DP table calculations. Note: we are not using any
+        // table here for storing values. Just using two
+        // variables will suffice.
+        for (int i = N - 2; i >= 0; --i) {
+            
+            // Same as the recursive solution.
+            int current = max(robNext, robNextPlusOne + nums[i]);
+            
+            // Update the variables
+            robNextPlusOne = robNext;
+            robNext = current;
+        }
+        
+        return robNext;
+    }
+};
