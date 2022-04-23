fa6f7a9d5d88e2e3d9c3ca76b39c85942051c53f
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2022-04-23 07:43:08 +0530

    Time: 6 ms (50.25%), Space: 42.2 MB (88.19%) - LeetHub

---
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.java
@@ -0,0 +1,17 @@
+public class Codec {
+     Map<Integer, String> mp = new HashMap<>();
+    int i = 0;
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        mp.put(i,longUrl);
+        return "http://tinyurl.com/"+i++;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return mp.get(Integer.parseInt(shortUrl.replace("http://tinyurl.com/", "")));
+    }
+}
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(url));
