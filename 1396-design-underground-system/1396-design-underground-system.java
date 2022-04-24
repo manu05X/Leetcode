7030dad8faae153e5cb62305c6e84bb861e32e9b
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2022-04-24 10:34:00 +0530

    Time: 105 ms (79.59%), Space: 55.4 MB (90.38%) - LeetHub

---
+++ 1396-design-underground-system/1396-design-underground-system.java
@@ -0,0 +1,49 @@
+class UndergroundSystem {
+    private Map<String, Pair<Integer, Integer>> journeyData = new HashMap<>();
+    private Map<Integer, Pair<String, Integer>> checkInData = new HashMap<>();
+    
+    public UndergroundSystem() {
+        
+    }
+    
+    public void checkIn(int id, String stationName, int t) {
+        checkInData.put(id, new Pair<>(stationName,t));
+    }
+    
+    public void checkOut(int id, String stationName, int t) {
+          // Look up the check in station and check in time for this id.
+        Pair<String, Integer> checkInId = checkInData.get(id);
+        String startStation = checkInId.getKey();
+        Integer checkInTime = checkInId.getValue();
+        
+         // Lookup the current travel time data for this route.
+        String route = startStation + "-" + stationName;
+        Pair<Integer, Integer> routeStats = journeyData.getOrDefault(route, new Pair<>(0,0));
+        Integer totalTripTime = routeStats.getKey();
+        Integer totalTrips = routeStats.getValue();
+        
+        // Update the travel time data with this trip.
+        Integer tripTime = t - checkInTime;
+        journeyData.put(route, new Pair<>(tripTime + totalTripTime, totalTrips+1));
+        
+    }
+    
+    public double getAverageTime(String startStation, String endStation) {
+        String route = startStation + "-" + endStation;
+        
+        Pair<Integer, Integer> checkout = journeyData.get(route);
+        Integer totalTime = checkout.getKey();
+        Integer totalTrips = checkout.getValue();
+        
+        return (double) totalTime / totalTrips;
+        
+    }
+}
+
+/**
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * UndergroundSystem obj = new UndergroundSystem();
+ * obj.checkIn(id,stationName,t);
+ * obj.checkOut(id,stationName,t);
+ * double param_3 = obj.getAverageTime(startStation,endStation);
+ */
