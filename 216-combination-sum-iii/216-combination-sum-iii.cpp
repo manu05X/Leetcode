3b46914d46797dd6c401ef766fc897d82657b33c
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2022-05-10 08:31:02 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (58.80%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void SumUntil(vector<vector<int>>& ans, vector<int>& combination, int k, int target, int start){
+        if(combination.size() == k && target == 0)
+        {
+            ans.push_back(combination);
+            return;
+        }
+        if(combination.size() < k)
+        {
+          for(int i = start; i <= 9; i++)
+          {
+                if(target - i < 0)
+                    break;
+                combination.push_back(i);
+                SumUntil(ans, combination, k, target-i,i+1);
+                combination.pop_back();
+            }  
+        }
+    }
+    
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> ans;
+        vector<int> combination;
+
+        SumUntil(ans,combination,k,n,1);
+        return ans;
+    }
+};
