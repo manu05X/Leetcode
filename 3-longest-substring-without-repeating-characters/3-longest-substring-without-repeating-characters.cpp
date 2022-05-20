d0513c543505eaff09c8e42d480115cb8c141aa4
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2022-05-20 11:22:48 +0530

    Update 3-longest-substring-without-repeating-characters.cpp

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,3 +1,68 @@
+//Approach 1: Brute Force
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.length();
+
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (checkRepetition(s, i, j)) {
+                    res = max(res, j - i + 1);
+                }
+            }
+        }
+
+        return res;
+    }
+
+    bool checkRepetition(string& s, int start, int end) {
+        vector<int> chars(128);
+
+        for (int i = start; i <= end; i++) {
+            char c = s[i];
+            chars[c]++;
+            if (chars[c] > 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+//Time complexity : O(n3)
+----------------------------------------------------------------------------------------------------------------------------------------------------
+//Approach 2: Sliding Window
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> chars(128);
+
+        int left = 0;
+        int right = 0;
+
+        int res = 0;
+        while (right < s.length()) {
+            char r = s[right];
+            chars[r]++;
+
+            while (chars[r] > 1) {
+                char l = s[left];
+                chars[l]--;
+                left++;
+            }
+
+            res = max(res, right - left + 1);
+
+            right++;
+        }
+
+        return res;
+    }
+};
+//Time complexity : O(2n) -> O(n)
+----------------------------------------------------------------------------------------------------------------------------------------------------
+//Approach 3: Sliding Window Optimized
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
@@ -21,3 +86,45 @@ public:
         return maxLen;
     }
 };
+//Time complexity : O(n)
+/*
+Java (Assuming ASCII 128)
+
+The previous implements all have no assumption on the charset of the string s.
+
+If we know that the charset is rather small, we can replace the Map with an integer array as direct access table.
+
+Commonly used tables are:
+
+int[26] for Letters 'a' - 'z' or 'A' - 'Z'
+int[128] for ASCII
+int[256] for Extended ASCII
+*/
+----------------------------------------------------------------------------------------------------------------------------------------------------
+//
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        // we will store a senitel value of -1 to simulate 'null'/'None' in C++
+        vector<int> chars(128, -1);
+
+        int left = 0;
+        int right = 0;
+
+        int res = 0;
+        while (right < s.length()) {
+            char r = s[right];
+
+            int index = chars[r];
+            if (index != -1 and index >= left and index < right) {
+                left = index + 1;
+            }
+            res = max(res, right - left + 1);
+
+            chars[r] = right;
+            right++;
+        }
+        return res;
+    }
+};
+//Time complexity : O(n)
