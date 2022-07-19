fac270ab47de25610c41d8cd0a4445e376ea1f75
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2022-07-19 10:13:29 +0530

    Time: 3 ms (38.93%), Space: 6.6 MB (31.97%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -4,7 +4,7 @@ public:
        vector<vector<int>> v(numRows);
         for(int i = 0; i < numRows; i++)
         {
-            v[i].resize(i+1);
+            v[i].resize(i+1); // increasing the size of each row
             v[i][0] = v[i][i] = 1;
             
             for(int j = 1; j < i; j++)
