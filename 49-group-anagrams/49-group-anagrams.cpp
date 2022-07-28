425f38d82954ea336c59d5c9f2d925a2295199ba
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2022-07-29 02:01:56 +0530

    Time: 42 ms (82.18%), Space: 20.5 MB (60.83%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -5,13 +5,13 @@ public:
         for(string s : strs)
         {
             string t = s;
-            sort(t.begin(), t.end());
-            mp[t].push_back(s);
+            sort(t.begin(), t.end());// Sorting the string
+            mp[t].push_back(s); // Sorted string is the key and the value is the initial string
         }
         vector<vector<string>> ans;
         for(auto p : mp)
         {
-            ans.push_back(p.second);
+            ans.push_back(p.second); // Traversing the map and adding the vectors of string to ans
         }
         
         return ans;
