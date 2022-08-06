bbcf8868e13ca609d041b18821768bca79da5319
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2022-08-06 11:19:52 +0530

    Time: 123 ms (33.84%), Space: 33.7 MB (48.75%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
@@ -15,3 +15,13 @@ public:
         return ans;
     }
 };
+/*
+i = 0, [4,3,2,-7,8,2,3,1]      // change the sign of element at 4th index
+i = 1, [4,3,-2,-7,8,2,3,1]     // change the sign of element at 3rd index
+i = 2, [4,-3,-2,-7,8,2,3,1]    // change the sign of element at 2nd index
+i = 3, [4,-3,-2,-7,8,2,-3,1]   // change the sign of element at 7th index
+i = 4, [4,-3,-2,-7,8,2,-3,-1]   // change the sign of element at 8th index
+i = 5, [4,3,-2,-7,8,2,-3,-1]    // here before changing the sign of element at 2nd index we will realize that it is already negative, which implies that 2 has appeared before in the array. So, we will push 2 in the ans
+i = 6, [4,3,2,-7,8,2,-3,-1]     // again we will encounter a negative element at 3rd index and we will push 3 into our ans
+i = 7, [-4,3,-2,-7,8,2,-3,-1]   // change the sign of element at 1st index
+*/
