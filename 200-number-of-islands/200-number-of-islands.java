187bff956883f7b78153d14a7abc88a7dc6a84b8
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2022-08-29 22:36:22 +0530

    Time: 9 ms (23.73%), Space: 57.4 MB (55.32%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,37 +1,37 @@
 class Solution {
-    public void dfs(char[][] grid, int r, int c){
-        int nR = grid.length;
-        int nC = grid[0].length;
+    void dfs(char[][] grid, int r, int c)
+    {
+        int nr = grid.length;
+        int nc = grid[0].length;
         
-        if(r<0 || c<0 || r >= nR || c >= nC || grid[r][c] == '0'){
+        if(r < 0 || r >= nr || c < 0 || c >= nc || grid[r][c] == '0')
             return;
-        }
-        grid[r][c] = '0';
-        dfs(grid, r - 1, c);
-        dfs(grid, r + 1, c);
-        dfs(grid, r, c - 1);
-        dfs(grid, r, c + 1);
-    }
-    public int numIslands(char[][] grid) {
-        if(grid == null || grid.length == 0)
-            return 0;
         
-        int nR = grid.length;
-        int nC = grid[0].length;
+        grid[r][c] = '0';
+        dfs(grid, r+1,c);
+        dfs(grid, r-1,c);
+        dfs(grid, r,c+1);
+        dfs(grid, r,c-1);
+    }
+    
+    public int numIslands(char[][] grid) 
+    {
+        int nr = grid.length;
+        int nc = grid[0].length;
         
         int count = 0;
         
-        for(int r = 0; r < nR; r++)
+        for(int i = 0; i < nr; i++)
         {
-            for(int c = 0; c < nC; c++)
+            for(int j = 0; j < nc; j++)
             {
-                if(grid[r][c] == '1')
+                if(grid[i][j] == '1')
                 {
                     count++;
-                    dfs(grid,r,c);
+                    dfs(grid,i,j);
                 }
             }
         }
-        return count;
+        return count++;
     }
 }
