991fbbe8356426c2fe59d1c5a3b981e926a7a2a2
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2022-09-23 06:35:13 +0530

    Time: 15 ms (46.18%), Space: 13.7 MB (63.92%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,54 +1,50 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int firstOccurrance = findBound(nums, target, true);
-        if(firstOccurrance == -1)
-        {
-            return {-1,-1};
-        }
-        int lastOccurance = findBound(nums, target, false);
-        
-        return {firstOccurrance,lastOccurance};
-    }
-    
-    int findBound(vector<int> nums, int target, bool isFirst)
-    {
         int n = nums.size();
-        int l = 0, h = n-1;
+        int l = 0;
+        int h = n-1;
         
-        while(l <= h)
+        vector<int> ans(2,-1);
+        if(n < 1)
+            return ans;
+        while(l < h)
         {
             int m = (l+h)/2;
-            if(nums[m] == target)
+            if(nums[m] < target)
             {
-                if(isFirst)
-                {
-                    // condition for lower bound
-                    if(m == l || nums[m-1] != target)
-                    {
-                        return m;
-                    }
-                    h = m - 1;
-                }
-                else
-                {
-                    // condition for upper bound
-                    if(m == h || nums[m+1] != target)
-                    {
-                        return m;
-                    }
-                    l = m + 1;
-                }
-            }
-            else if(nums[m] > target)
-            {
-                h = m - 1;
+                l = m+1;
             }
             else
             {
-                l = m + 1;
+                h = m;
             }
         }
-        return -1;
+        
+        if(nums[l] != target)
+        {
+            return ans;
+        }
+        else
+        {
+            ans[0] = l;
+        }
+        
+        h = n-1;
+        while(l < h)
+        {
+            int m = (l+h)/2 + 1;
+            if(nums[m] > target)
+            {
+                h = m-1;
+            }
+            else
+            {
+               l = m;
+            }
+        }
+        ans[1] = h;
+        
+        return ans;
     }
 };
