5f6821b83d92d762c001353b1dc3a1896757a8b4
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2022-09-24 14:45:12 +0530

    Time: 6 ms (65.16%), Space: 12.1 MB (36.52%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-         int n = nums.size();
-        if (n < 2) 
-            return;
-        
-        int i = n-1, j = n-1;
-        
-        // Find the largest index i such that nums[i] < nums[i + 1]:
-        while (i > 0 && nums[i-1] >= nums[i]) 
-            i--;
-        
-        // If no such index exists - reverse the array and were done:
-        if (i == 0) 
-        {
-            reverse(nums.begin(), nums.end());
-            return;
+        int n = nums.size(), k, l;
+    	for (k = n - 2; k >= 0; k--) {
+            if (nums[k] < nums[k + 1]) {
+                break;
+            }
         }
-        
-        // Find the largest index j > i such that nums[i] < nums[j]:
-        i--;
-        while (j > i && nums[j] <= nums[i]) 
-            j--;
-        
-        // Swap nums[i] and nums[j]:
-        swap(nums[i], nums[j]);
-        
-        // Reverse the array from index i until the end:
-        reverse(nums.begin() + i + 1, nums.end());
+    	if (k < 0) {
+    	    reverse(nums.begin(), nums.end());
+    	} else {
+    	    for (l = n - 1; l > k; l--) {
+                if (nums[l] > nums[k]) {
+                    break;
+                }
+            } 
+    	    swap(nums[k], nums[l]);
+    	    reverse(nums.begin() + k + 1, nums.end());
+        }
     }
 };
