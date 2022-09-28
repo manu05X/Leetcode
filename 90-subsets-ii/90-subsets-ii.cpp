9962bd5efb35e9eaca485882e7542b23dfbb4139
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2022-09-28 07:35:37 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (96.36%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
-    void subsetUntil(vector<int>& nums,vector<int>& subset,vector<vector<int>>& ans, int left)
-{   
-        // for(int i = 0; i < ans.size(); i++)
-        // {
-        //     for(int j = 0; j < subset.size(); j++)
-        //     {
-        //         if(ans[i][j] == )
-        //     }
-        // }
-            ans.push_back(subset);
+    void dfs(vector<vector<int>>& ans, vector<int>& subset,vector<int>& nums, int left){
+        ans.push_back(subset);
         
-    for(int i = left; i < nums.size(); i++)
-    {
-        if(i == left || nums[i] != nums[i-1])
+        for(int i = left; i < nums.size(); i++)
         {
-            subset.push_back(nums[i]); // including nums[i]
-            subsetUntil(nums,subset,ans,i+1); // move to next element
-            subset.pop_back(); // exclude nums[i] from subset and trigger backtracking
-        }
+            if(i == left || nums[i] != nums[i-1])
+            {
+                subset.push_back(nums[i]);
+                dfs(ans,subset,nums,i+1);
+                subset.pop_back();
+            }  
+        }    
     }
-    //return;
-}
+    
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>> ans;
-        
         vector<int> subset;
-        sort(nums.begin(), nums.end());
-        subsetUntil(nums,subset,ans, 0);
-
+        sort(nums.begin(),nums.end());
+        dfs(ans,subset,nums,0);
+        
         return ans;
     }
 };
