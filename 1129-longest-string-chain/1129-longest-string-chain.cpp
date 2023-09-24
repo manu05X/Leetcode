b1307bc95e19e72bf01302c8e27aee41901d80cb
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2023-09-24 16:48:34 +0530

    Time: 63 ms (94.29%), Space: 17.7 MB (47.41%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.cpp
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -7,28 +7,55 @@ public:
     int longestStrChain(vector<string>& words) 
     {
         sort(words.begin(), words.end(),compare);
+         sort(words.begin(), words.end(),compare);
 
         unordered_map<string, int> mp;
-        int res = 1;
+        int maxLength = 0;
 
         for(string w : words)
         {
+            mp[w] = 1;
             for(int i = 0; i < w.length(); i++){
                 string pre = w.substr(0,i) + w.substr(i+1);
-
-                //mp[w] = max(mp[w], mp.find(pre) == mp.end() ? 1 : mp[pre] + 1);
 
                 if (mp.find(pre) != mp.end()) 
                 {
                     mp[w] = max(mp[w], mp[pre] + 1);
                 }
-                else
-                {
-                    mp[w] = max(mp[w], 1);
-                }
             }
-            res = max(res, mp[w]);
+            maxLength = max(maxLength, mp[w]);
         }
-        return res;
+        return maxLength;
     }  
+
+/*        
+    unordered_map<string, int> dp; // Store the longest chain length for each word.
+
+    int maxLength = 1; // Initialize the maximum chain length to 1.
+
+    for (const string& word : words) 
+    {
+        // Initialize the chain length for the current word to 1.
+        dp[word] = 1;
+
+        // Try removing each character from the word to find predecessors.
+        for (int i = 0; i < word.length(); ++i) 
+        {
+            string predecessor = word.substr(0, i) + word.substr(i + 1);
+
+            if (dp.find(predecessor) != dp.end()) 
+            {
+                // Update the chain length if a predecessor is found.
+                dp[word] = max(dp[word], dp[predecessor] + 1);
+            }
+        }
+
+        // Update the maximum chain length.
+        maxLength = max(maxLength, dp[word]);
+    }
+
+    return maxLength;
+    }  
+
+    */
 };
