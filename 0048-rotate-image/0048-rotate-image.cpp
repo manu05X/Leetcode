d5520a25c245b292656805448da1deb5ef57268b
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2023-10-07 08:07:37 +0530

    Time: 0 ms (100%), Space: 7.4 MB (13.74%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& mat) {
+        int n = mat.size();
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = i+1; j < n; j++)
+            {
+                swap(mat[i][j],mat[j][i]);
+            }
+        }
+        
+        for(int i =0; i < n; i++)
+        {
+            int l = 0;
+            int h = n-1;
+            
+            while(l < h)
+            {
+                swap(mat[i][l],mat[i][h]);
+                l++;
+                h--;
+            }
+        }
+    }
+};
+/*
+0. 1. 2
+[1,2,3] 0
+[4,5,6] 1 
+[7,8,9] 2
+
+  |
+  V
+  
+1,4,7
+2,5,8
+3,6,9
+
+*/
