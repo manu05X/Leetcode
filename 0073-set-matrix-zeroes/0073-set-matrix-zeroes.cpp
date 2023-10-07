4154b5deadf1cf087160ff1b2db8187e95bff962
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2023-10-07 09:07:57 +0530

    Time: 12 ms (55.51%), Space: 13.8 MB (29.13%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        bool colmn0 = false;
+        for(int i = 0; i < n; i++){
+            // checking if 0 is present in 0th column or not
+            if(matrix[i][0] == 0){
+                colmn0=true;
+            }
+            for(int j = 1; j < m; j++){
+                if(matrix[i][j] == 0)
+                {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // traversing in reverse direction
+        for(int i = n-1; i >= 0; i--)
+        {
+            for(int j = m-1; j >= 1; j--)
+            {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+            // putting 0 at matrix[0][0] if col0 is false
+            if(colmn0 == true)
+            {
+                matrix[i][0] = 0;
+            }
+        }
+
+        
+    }
+};
