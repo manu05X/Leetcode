b40717f94624f75d332b76bef11714b750e35ab8
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2023-10-10 23:34:46 +0530

    Time: 358 ms (24.48%), Space: 110.2 MB (5.23%) - LeetHub

---
+++ 2119-minimum-number-of-operations-to-make-array-continuous/2119-minimum-number-of-operations-to-make-array-continuous.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int n = nums.size();
+        //Set the answer ans = n.
+        int ans = n;
+        //Remove duplicates from nums and then sort it.
+        set<int> unique(nums.begin(), nums.end());
+        vector<int> newNums;
+        //We will call this new array newNums
+        for(int num : unique){
+            newNums.push_back(num);
+        }
+
+        //initialize the iterator over the indices of newNums
+        int j = 0;
+        for(int i = 0; i < newNums.size(); i++){
+            //While newNums[j] is within our range (less than newNums[i] + n), increment j
+            while(j < newNums.size() && newNums[j] < newNums[i]+n){
+                j++;
+            }
+
+            //the number of elements already in our range
+            int count = j-i;
+            //Update ans with n - count if it is smaller.
+            ans = min(ans, n-count);
+        }
+
+        return ans;
+        
+    }
+};
