37fd511baa93a9fded4ce3fb67e3523920a65999
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2023-10-11 20:49:20 +0530

    Time: 3 ms (42.92%), Space: 6.9 MB (17.88%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        int ns = s.size();
+        int np = pattern.size();
+        unordered_map<char, string> map_char;
+        unordered_map<string, char> map_word;
+
+        vector<string> word;
+        string tmp = "";
+        for(int i = 0; i < ns; i++){
+            //cout << s[i] <<" "<<endl;
+            if(s[i] == ' '){
+                word.push_back(tmp);
+                tmp = "";
+            }
+            else{
+                //cout << s[i] <<" "<<endl;
+                tmp += s[i];
+            }
+        }
+        word.push_back(tmp);
+
+        if (word.size() != pattern.size())
+            return false;
+
+        for(int i = 0; i < word.size(); i++)
+        {
+            char c = pattern[i];
+            string w = word[i];
+
+            if(map_char.find(c) == map_char.end()){
+                if(map_word.find(w) != map_word.end()){
+                    return false;
+                }
+                else
+                {
+                    map_word[w] = c;
+                    map_char[c] = w;
+                }
+            }
+            else
+            {
+                string mapped_word = map_char[c];
+                if(mapped_word != w){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
+
+/*
+Some what similar
+
+https://leetcode.com/problems/isomorphic-strings/description/?envType=study-plan-v2&envId=top-interview-150
+
+
+
+*/
