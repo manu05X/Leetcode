06816de9ce06b0091d973a3792577ab2067be27f
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2023-10-12 19:14:42 +0530

    Time: 34 ms (40.36%), Space: 21.3 MB (29.56%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -4,13 +4,17 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         cout.tie(NULL);
-        
+
+        //map of string and vector<string>
         unordered_map<string, vector<string> > m;
         vector<vector<string>> ans;
 
+        // add string to map
         for(auto str : strs){
             string s = str;
+            // sort the string i.e s
             sort(s.begin(), s.end());
+            //push_back map with key s and value from str
             m[s].push_back(str);
         }
 
