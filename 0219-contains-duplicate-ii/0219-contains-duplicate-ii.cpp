15501df158c00d6f4f20292f90a018c5e3380ad7
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2023-10-14 12:57:55 +0530

    Time: 104 ms (98.85%), Space: 77.9 MB (28.42%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -4,20 +4,25 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         cout.tie(NULL);
-        
+
         unordered_map<int,int> mp;
         int n = nums.size();
 
         for(int i = 0; i < n; i++)
         {
+            //map.count returns 1 if the element with key K is present in the map container. It returns 0 if the element with key K is not present in the container.
+
             if(mp.count(nums[i]))
             {
-                //map.count returns 1 if the element with key K is present in the map container. It returns 0 if the element with key K is not present in the container.
+                //last index of k in map
+                int kIndex = mp[nums[i]];
+                // abs diffrence curr idex and kIndex
+                int diff = abs(i-kIndex);
 
-
-                if(abs(i-mp[nums[i]]) <= k)
+                if(diff <= k)
                     return true;
             }
+            // inserting element in map
             mp[nums[i]] = i;
         }
         return false;
