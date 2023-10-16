b897d64760f1811e8912d179192784ca3bd22131
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2023-10-16 08:46:35 +0530

    Time: 0 ms (100%), Space: 7.1 MB (17.54%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> ans(numRows);
+        // talking 2 D vector
+        vector<vector<int>> ans(numRows); 
 
         for(int i = 0; i < numRows; i++){
+            //Resize the no of colmn according to no of row
             ans[i].resize(i+1);
+            // defining its first and last element as 1
             ans[i][0] = ans[i][i] = 1;
 
+            // filling column sum 
             for(int j = 1; j < i; j++){
                 ans[i][j] = ans[i-1][j-1] + ans[i-1][j];
             }
