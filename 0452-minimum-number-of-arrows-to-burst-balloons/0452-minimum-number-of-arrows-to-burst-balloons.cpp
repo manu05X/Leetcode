8b0cf388f6515ec44b9f22e60e5578df1da53f5e
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2023-10-18 19:46:46 +0530

    Time: 408 ms (20.66%), Space: 102.8 MB (14.4%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -14,9 +14,11 @@ public:
         // init cur_end with the first balloon
         int curr_end = points[0][1];
         for(auto p : points) {
+            // new_start
             int l = p[0];
+            //new_end
             int r = p[1];
-            // if cur_end < new_start
+            // if cur_end less than new_start
             if(curr_end < l){
                 // then we need a new arrow
                 ans += 1;
@@ -28,3 +30,18 @@ public:
 
     }
 };
+
+/*
+    0.   1.    2.     3. 
+[[1,6],[2,8],[7,12],[10,16]]
+
+i = 0,1,2,3
+
+l = 1,2,7,10
+r = 6,8,12,16
+curr_end = 6,6,12,12
+ans = 1,1,2,2
+
+return ans = 1
+
+*/
