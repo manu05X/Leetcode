6ea2e75fa6cb54244b1d1c1f863db268b900468d
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2023-10-27 18:28:08 +0530

    Time: 6 ms (96.99%), Space: 7.3 MB (79.22%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
-    // returns size of the current/latest palindrome
-int ReturnPalindromeLength(string& s, int left, int right) {
-	while (left >= 0 && right < s.length() && s[left] == s[right]) {
-		left--;
-		right++;
-	}
-	return right - left - 1;
-}
 
-string longestPalindrome(string s) {
-	// Return an empty string if the original string itself
-	// is empty or null
-	if (s.empty())
-		return "";
+    int isPalindrome(string& s, int left, int right){
+        while(left >= 0 && right < s.size() && s[left] == s[right]){
+            left--;
+            right++;
+        }
+        return right-left-1;
+    }
+    string longestPalindrome(string s) {
+        int n = s.size();
+        if (s.empty())
+		    return "";
 
-	// Initializing start and end indexes of resultant substring
+        // Initializing start and end indexes of resultant substring
 	// as zero
-	int start = 0, end = 0;
+	    int start = 0, end = 0;
 
-	for (int i = 0; i < s.length(); i++) {
-		int len1 = ReturnPalindromeLength(s, i, i); // for odd length
-		int len2 = ReturnPalindromeLength(s, i, i + 1); // for even length
-		int len = max(len1, len2);
+        for(int i = 0; i < n; i++){
+            int len1 = isPalindrome(s,i,i); // for odd length
+            int len2 = isPalindrome(s,i,i+1); // for even length
 
-		if (len > end - start) {
-			// Updating start and end indexes of palindromic substring
+            int len = max(len1,len2);
+
+            if(len > end-start){
+                // Updating start and end indexes of palindromic substring
 			start = i - (len - 1) / 2;
 			end = i + len / 2;
-		}
-	}
+            }
+        }
 
-	// Returning the longest palindromic substring
-	return s.substr(start, end + 1 - start);
-}
+        return s.substr(start, end+1-start);
+    }
 };
