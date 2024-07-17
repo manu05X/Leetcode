0f0ebe0ff57e61cfe2639c800f073e4299975d5c
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-07-17 12:43:08 +0530

    Time: 19 ms (27.13%), Space: 7.3 MB (24.39%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.go
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.go
@@ -1,3 +1,16 @@
+func isPalindrome(s string, left, right int) bool {
+    for left < right {
+        if s[left] != s[right] {
+            return false
+        }
+
+        left++
+        right--
+    }
+
+    return true
+}
+
 func validPalindrome(s string) bool {
     left, right := 0, len(s)-1
 
@@ -11,15 +24,3 @@ func validPalindrome(s string) bool {
     return true
 }
 
-func isPalindrome(s string, left, right int) bool {
-    for left < right {
-        if s[left] != s[right] {
-            return false
-        }
-
-        left++
-        right--
-    }
-
-    return true
-}
