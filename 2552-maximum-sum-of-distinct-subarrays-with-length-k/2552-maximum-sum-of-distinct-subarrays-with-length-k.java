a851c8d258ecf32e3ee371f10b9dd79147d978aa
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-07-27 00:16:28 +0530

    Time: 45 ms (47.47%), Space: 59.2 MB (24.35%) - LeetHub

---
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -0,0 +1,33 @@
+class Solution {
+    public long maximumSubarraySum(int[] nums, int k) {
+        int n = nums.length;
+        long maxSum = 0;
+        long currWindowSum = 0;
+
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        int left = 0;
+
+        for(int right = 0; right < n; right++){
+            currWindowSum += nums[right];
+            mp.put(nums[right], mp.getOrDefault(nums[right],0)+1);
+            
+            int currWindowLength = right-left+1;
+            while(currWindowLength > k)
+            {
+                currWindowSum -= nums[left];
+                mp.put(nums[left], mp.getOrDefault(nums[left],0)-1);
+
+                if(mp.get(nums[left]) == 0){
+                    mp.remove(nums[left]);
+                }
+                currWindowLength--;
+                left++;
+            }
+            // Update maxSum if the window size is k and all elements are distinct
+            if (currWindowLength == k && mp.size() == k) {
+                maxSum = Math.max(maxSum, currWindowSum);
+            }
+        }
+        return maxSum;
+    }
+}
