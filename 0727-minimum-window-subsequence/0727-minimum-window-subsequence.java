d098e4e3b1e3257eb8b403773a5b61cfb33516f4
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-07-29 13:50:23 +0530

    Time: 8 ms (86.81%), Space: 44.6 MB (58.28%) - LeetHub

--- 0727-minimum-window-subsequence/0727-minimum-window-subsequence.java
+++ 0727-minimum-window-subsequence/0727-minimum-window-subsequence.java
@@ -8,15 +8,20 @@ class Solution {
         int minLength = Integer.MAX_VALUE;
         String ans = "";
 
-        while(p1 < n1){
-            if(s1.charAt(p1) == s2.charAt(p2)){
+        // iterate over str1 while we haven't reached the end of pointer one.
+        while(p1 < n1)
+        {
+            if(s1.charAt(p1) == s2.charAt(p2))
+            {
+                // increment to find the next matching element.
                 p2++;
-
+                // check if a valid substring has been found
                 if(p2 == n2){
                     p2--;
-
+                    // here we will stretch the window from start to end by decrementing the size of start;
                     int start = p1, end = p1+1;
-                    
+                    // we'll iterate backwards to find all the chars of s2
+                    // abced ace
                     while(p2 >= 0){
                         if(s1.charAt(start) == s2.charAt(p2)){
                             p2--;
@@ -25,18 +30,19 @@ class Solution {
                     }
                     start +=1;
 
-                    if(minLength > end-start){
+                    // update minSubLen and minSubsequence if current subsequence is shorter
+                    if(minLength > end-start)
+                    {
                         minLength = end-start;
                         ans = s1.substring(start, end);
                     }
+                    // reassigning the p1 and p2
                     p1 = start+1;
                     p2 = 0;
                 }
             }
             p1++;
         }
-
         return ans;
-        
     }
 }
