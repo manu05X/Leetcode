86ebd93b82d6018d51d623bb1f777d787c97bfa3
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-07-30 09:29:15 +0530

    Time: 75 ms (91.23%), Space: 23.8 MB (65.23%) - LeetHub

--- 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
@@ -5,13 +5,14 @@ public:
         int n = s.size(), bCount = 0;
         int deletCount = 0;
 
+        //aababbab
         for(int i = 0; i < n; i++){
-            char x = s[i];
+            char x = s[i]; //a,a,b,a,b,b,a,b
 
             if(x == 'b'){
-                bCount++;
+                bCount++; //1,2,3,4
             } else {
-                deletCount = min(deletCount+1, bCount);
+                deletCount = min(deletCount+1, bCount); //0,0,1,2,
             }
         }
 
