1e4e08a7e12988909eb788ee9d33a75b46e52776
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-07-31 15:13:23 +0530

    Time: 43 ms (90.86%), Space: 38.3 MB (67.04%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,26 +1,32 @@
 class Solution {
     // Dezerve interview
 public:
-    int leastInterval(vector<char>& tasks, int n) {
-        int x = tasks.size();
-        unordered_map<char, int> mp;
-        int count = 0;
-        
-        for(int i = 0; i < x; i++){
-            mp[tasks[i]]++;
-            count = max(count, mp[tasks[i]]);
+    int leastInterval(vector<char>& tasks, int coolingPeriod) {
+        int taskCount[26] = {0}; // Initialize a vector to keep count of each task
+        int maxCount = 0; // Variable to keep track of the maximum count of a single task
+
+        // Count the tasks and find out the task with the maximum count
+        for (char task : tasks) {
+            task = task - 'A'; // Convert char to an index between 0 and 25
+            ++taskCount[task]; // Increment the count for this task
+            maxCount = max(maxCount, taskCount[task]); // Update maxCount if current task's count is greater
         }
-        int result = (count-1) * (n+1);
-        //cout <<"From solve - " <<result <<endl;
-        for(int i = 0; i < mp.size(); i++){
-            if(mp[i] == count){
-                result++;
+
+        // Count how many tasks have the same count as maxCount
+        int tasksWithMaxCount = 0;
+        for (int count : taskCount) {
+            if (count == maxCount) {
+                ++tasksWithMaxCount;
             }
         }
-        //cout <<"From - " <<result <<endl;
-        
-        return max(result,x);
-    
 
+        // Calculate the least interval
+        // First part: Calculate the minimum slots required based on the most frequent task (maxCount - 1) times (coolingPeriod + 1)
+        // Second part: Add the number of tasks that have the highest frequency (taskWithMaxCount)
+        int minSlotsRequired = (maxCount - 1) * (coolingPeriod + 1) + tasksWithMaxCount;
+
+        // The result is the maximum between the actual size of tasks and the minimum slots required
+        return max(((int)tasks.size()), minSlotsRequired);
     }
 };
+//Dezerve 
