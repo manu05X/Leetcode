8c90c3b9fadc7a0c16c76488fc2143bc8f5cd4a5
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-08-02 12:37:37 +0530

    Time: 0 ms (100%), Space: 42.3 MB (8.93%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -4,21 +4,23 @@ class Solution {
         // 'left' is the position where the next 0 should go,
         // 'right' is the position one more than where the next 2 should go,
         // 'current' is the current index being considered.
-        int left = -1, right = nums.length, current = 0;
+        int left = 0, right = nums.length-1, current = 0;
       
-        while (current < right) { // Process elements until 'current' reaches 'right'
+        while (current <= right) { // Process elements until 'current' reaches 'right'
             if (nums[current] == 0) {
                 // When a 0 is found, swap it with the element at 'left' position,
                 // then move both 'left' and 'current' one step right.
-                ++left;
+                
                 swap(nums, left, current);
                 current++;
+                ++left;
             } else if (nums[current] == 2) {
                 // When a 2 is found, swap it with the element just before 'right' position,
                 // then decrement 'right' to move it leftward.
                 // Note 'current' is not incremented because the swapped element needs to be checked.
-                --right;
+                
                 swap(nums, right, current);
+                --right;
             } else {
                 // If the element is 1, just move 'current' one step to the right.
                 ++current;
