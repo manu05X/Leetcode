741e2b484f319cb706cab9be39b5dad01014cc76
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-08-05 17:46:42 +0530

    Time: 30 ms (26.4%), Space: 136.3 MB (5.44%) - LeetHub

---
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    string removeDuplicates(string s, int k) {
+        int n = s.size();
+        stack<pair<char,int>> st;
+
+        for(int i = 0; i < n; i++){
+            char curr = s[i];
+
+            if(!st.empty())
+            {
+                if(st.top().first == curr)
+                {
+                    st.top().second += 1;
+                    if(st.top().second % k == 0)
+                    {
+                        st.pop();
+                    }
+                }
+                else{
+                    st.push({curr, 1});
+                }
+            }
+            else{
+                st.push({curr, 1});
+            }
+        }
+
+        string ans = "";
+        while(!st.empty())
+        {
+            ans = string(st.top().second, st.top().first)+ans;
+            st.pop();
+            // if(st.top().second == 0)
+            // {
+            //     st.pop();
+            // }
+            // else
+            // {
+            //     ans = st.top().first+ans;
+            //     st.top().second--;
+            // }
+        }
+
+        return ans;
+    }
+};
