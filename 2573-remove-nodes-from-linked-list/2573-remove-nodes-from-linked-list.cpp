ab59559d366d989d5185683a92bb2352e4703e67
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-08-05 17:10:21 +0530

    Time: 247 ms (92.9%), Space: 168.3 MB (44.82%) - LeetHub

--- 2573-remove-nodes-from-linked-list/2573-remove-nodes-from-linked-list.cpp
+++ 2573-remove-nodes-from-linked-list/2573-remove-nodes-from-linked-list.cpp
@@ -11,6 +11,9 @@
 class Solution {
 public:
     ListNode* removeNodes(ListNode* head) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
         if(head == NULL) return NULL;
 
         ListNode* curr = head;
