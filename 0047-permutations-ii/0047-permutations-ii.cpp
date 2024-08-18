dbb2375222c0fcfbd396c844b02d4e7b717b86dc
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-08-18 14:19:18 +0530

    Time: 3 ms (87.36%), Space: 10.7 MB (78.61%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+   void dfs(vector<int>& nums, int idx, vector<vector<int>>& ans) {
+        if (idx == nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+        
+        for (int i = idx; i < nums.size(); i++) {
+            if (i == idx || nums[i] != nums[idx]) {
+                swap(nums[idx], nums[i]);
+                dfs(nums, idx + 1, ans);
+            }
+        }
+        sort(nums.begin() + idx, nums.end());
+    }
+    
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        dfs(nums, 0, ans);
+        return ans;
+    }
+};
