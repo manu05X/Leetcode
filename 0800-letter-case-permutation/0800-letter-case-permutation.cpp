3aabb0a0dc277fc5e25adebd14b6bd9998ff8758
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-08-18 19:50:18 +0530

    Time: 4 ms (70.19%), Space: 13.8 MB (71.73%) - LeetHub

--- 0800-letter-case-permutation/0800-letter-case-permutation.cpp
+++ 0800-letter-case-permutation/0800-letter-case-permutation.cpp
@@ -24,3 +24,37 @@ public:
         }
     }
 };
+
+/*
+Understanding XOR Operation with ' '
+The ASCII value of space (' ') is 32 (in binary: 0010 0000).
+The XOR operation (^) flips bits where there is a 1.
+
+Example 1: Toggling from 'a' to 'A'
+'a': In ASCII, 'a' is represented as 01100001 in binary.
+
+ASCII value of 'a': 97 (in binary: 0110 0001)
+
+  01100001   // 'a'
+^ 00100000   // ' '
+------------
+  01000001   // 'A'
+
+
+Example 2: Toggling from 'A' to 'a'
+'A': In ASCII, 'A' is 01000001 in binary.
+**' '(space):00100000` in binary.
+Apply XOR:
+
+  01000001   // 'A'
+^ 00100000   // ' '
+------------
+  01100001   // 'a'
+The result is 'a', so charArray[idx] ^= ' ' converts 'A' to 'a'.
+
+
+Summary
+The XOR with space (' ') flips the 6th bit of the ASCII code.
+If the character is lowercase, it becomes uppercase, and vice versa.
+
+*/
