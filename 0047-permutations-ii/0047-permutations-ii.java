6c49069fedfbf9acb2430b37312ca0aa17cc6fae
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-08-25 09:43:32 +0530

    Time: 1 ms (99.85%), Space: 44.7 MB (63.05%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,35 +1,38 @@
 class Solution {
+    List<Integer> currTemp = new ArrayList<>();
     List<List<Integer>> ans = new ArrayList<>();
+    int[] elem;
+    boolean[] visited;
 
-    public void dfs(int[] nums, int idx) {
-        if (idx == nums.length) {
-            List<Integer> tmp = new ArrayList<>();
-            for (int x : nums) {
-                tmp.add(x);
-            }
-            ans.add(tmp);
+    public void dfs(int idx){
+        // Base case: if the permutation size is equal to the number of elements, add it to the answer
+        if(idx == elem.length){
+            ans.add(new ArrayList<>(currTemp));
             return;
         }
-
-        Set<Integer> used = new HashSet<>();
-        for (int i = idx; i < nums.length; i++) {
-            if (used.add(nums[i])) { // Check if nums[i] is already used in this position
-                swap(nums, i, idx);
-                dfs(nums, idx + 1);
-                swap(nums, i, idx); // Backtrack to restore the original state
+        
+        for(int i = 0; i < elem.length; ++i){
+            // Skip the current number if it's already been used or if it's a duplicate and the duplicate hasn't been used
+            if (visited[i] || (i > 0 && elem[i] == elem[i - 1] && !visited[i - 1])) {
+                continue;
+            }
+            if(!visited[i]){
+                visited[i] = true;
+                currTemp.add(elem[i]);
+                dfs(idx+1);
+                currTemp.remove(currTemp.size()-1);
+                 visited[i] = false;
             }
         }
-    }
-
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
     }
 
     public List<List<Integer>> permuteUnique(int[] nums) {
-        Arrays.sort(nums); // Sort initially to handle duplicates
-        dfs(nums, 0);
+        Arrays.sort(nums);
+        elem = nums;
+        visited = new boolean[nums.length];
+
+        dfs(0);
+
         return ans;
     }
 }
