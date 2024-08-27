b45077376e324e1e80c84abad945b0be0a0acb0f
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-08-27 09:25:32 +0530

    Time: 8 ms (24.95%), Space: 13 MB (41.43%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -7,16 +7,17 @@ public:
             return;
         }
 
-        int prevElm = INT_MIN; 
+        //int prevElm = INT_MIN; 
 
         // Iterate through the candidates starting from the 'start' index
         for (int i = start; i < candidates.size(); ++i) {
-            // // Skip duplicates to avoid duplicate combinations
-            // if (i > start && candidates[i] == candidates[i - 1]) {
-            //     continue;
-            // }
+            // Skip duplicates to avoid duplicate combinations
+            // This checks if the current element is the same as the previous one, and if it is, it skips it, but only when i > start.
+            if (i > start && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
 
-            if(candidates[i] == prevElm) continue;
+            //if(candidates[i] == prevElm) continue;
 
             // Check if the current candidate can be included in the combination
             if (candidates[i] <= target) {
@@ -28,7 +29,7 @@ public:
                 path.pop_back();
             }
 
-            prevElm = candidates[i];
+            //prevElm = candidates[i];
         }
     }
 
