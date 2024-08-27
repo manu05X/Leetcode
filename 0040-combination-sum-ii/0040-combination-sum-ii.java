a9ea6318e8bec501acbf1ebc80c1c36392b09f52
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-08-27 09:19:33 +0530

    Time: 3 ms (77.97%), Space: 43.8 MB (6.61%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -5,15 +5,19 @@ class Solution {
             return;// If remain becomes negative, terminate the current branch of the backtracking
         }
         else if(target == 0){
+            //Deep copy
             ans.add(new ArrayList<>(currSubarr));// If remain is zero, add the current combination to the result
         }
         else{
+            int prevElement = Integer.MIN_VALUE;
             for(int i = start; i < candidates.length; i++){
-                if(i > start && candidates[i] == candidates[i-1]) continue;
+                //if(i > start && candidates[i] == candidates[i-1]) continue;
+                if(prevElement == candidates[i]) continue;
                 if(candidates[i] <= target){
                     currSubarr.add(candidates[i]);// Choose the current candidate
                     dfs(candidates, target-candidates[i], currSubarr, i+1);// Recursively backtrack with updated remain
                     currSubarr.remove(currSubarr.size()-1);// Remove the last element to backtrack
+                    prevElement = candidates[i];
                 }
             }
         }
