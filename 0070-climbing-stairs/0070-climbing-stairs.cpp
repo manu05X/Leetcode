94d1a2c8e04f3f8c35007b6d5309b73a30ae29b5
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-08-29 23:51:59 +0530

    Time: 3 ms (29.64%), Space: 7.3 MB (78.85%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
     int climbStairs(int n) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
         if(n == 1){
             return 1;
         }
-
-        int first = 1;
-        int second = 2;
-        for(int i = 3; i <= n; i++){
-            int third = first+second;
-            first = second;
-            second = third;
+         // Initialize variables to store previous two results
+        int prevStep = 0; // Corresponds to the number of ways to climb to the previous step
+        int currentStep = 1; // Corresponds to the number of ways to climb to the current step
+      
+        // Loop through the number of stairs to calculate the number of ways
+        for (int i = 0; i < n; ++i) {
+            // Calculate the number of ways to climb to the next step
+            int nextStep = prevStep + currentStep; // Sum of the previous two results
+          
+            // Update variables for the next iteration
+            prevStep = currentStep; // The current step becomes the previous step for the next iteration
+            currentStep = nextStep; // The calculated next step becomes the current step for the next iteration
         }
-        return second;
+      
+        // Return the number of ways to climb to the top step
+        return currentStep;
     }
 };
