d896773c4485525d2cb5e173105c21cdf75e8dd9
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-08-31 16:47:13 +0530

    Time: 81 ms (94.74%), Space: 111.2 MB (96.44%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,3 +1,18 @@
+/*
+Analysis
+
+To solve this problem, we need to understand and use the following 2 facts:
+1) if the sum of gas >= the sum of cost, then the circle can be completed.
+2) if A can not reach C in a the sequence of A-->B-->C, then B can not make it either.
+
+Proof of fact 2:
+
+If gas[A] < cost[A], then A can not even reach B. 
+So to reach C from A, gas[A] must >= cost[A]. 
+Given that A can not reach C, we have gas[A] + gas[B] < cost[A] + cost[B],
+and gas[A] >= cost[A],
+Therefore, gas[B] < cost[B], i.e., B can not reach C. 
+*/
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
@@ -29,3 +44,10 @@ public:
         }
     }
 };
+/*
+i           = _, 0, 1, 2, 3, 4
+remaning    = 0, -2,-2,-2,3, 3
+sumRemaning = 0, -2,-2,-2,3, 6
+start       = 0, 0, 1, 2, 3, 3
+total       = 0, -2,-4,-6,-3,0
+*/
