3f5674632f228eba9498ae0a2d6f388bf26adeb0
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-08-31 09:16:16 +0530

    Time: 6 ms (82.37%), Space: 16.6 MB (56.74%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-        
-        vector<int> a(1e4+1, 0);
-        int maxi = *max_element(nums.begin(), nums.end());
-        for(auto i: nums) a[i]+=i;
+        // Step 1: Create a frequency array (or count array) of size 10,001.
+        vector<int> points(1e4 + 1, 0);
 
-        vector<int> dp(1e4+1, 0);
-        dp[1] = a[1];
-
-        for(int i=2; i<maxi+1; i++)
-        {
-            dp[i] = max(dp[i-1], a[i] + dp[i-2]);
+        // Step 2: Calculate the total points for each number
+        for (auto num : nums) {
+            points[num] += num;
         }
 
-        return dp[maxi];
+        // Step 3: Initialize two variables: 'exc' and 'inc'.
+        int excludeCurrent = 0;
+        int includeCurrent = 0;
+
+        // Iterate through all possible numbers (1 to 10000)
+        for (int i = 0; i <= 10000; i++) {
+            // Step 4: Iterate through the frequency array and calculate maximum points.
+            int tempInclude = excludeCurrent + points[i]; // Points if current number is included
+            int tempExclude = max(excludeCurrent, includeCurrent); // Points if current number is excluded
+
+             // Update 'exc' to the maximum points excluding current number
+            excludeCurrent = tempExclude;
+            includeCurrent = tempInclude;// Update 'inc' to the points including current number
+        }
+
+        return max(excludeCurrent, includeCurrent);
     }
 };
