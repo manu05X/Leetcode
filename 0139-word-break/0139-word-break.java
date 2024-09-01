3dbd2b1fd47561eb06a66919ea3803c094aa59be
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-09-01 08:56:55 +0530

    Time: 10 ms (15.05%), Space: 44.7 MB (17.44%) - LeetHub

---
+++ 0139-word-break/0139-word-break.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordDictSet = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                // char x = s.charAt(j);
+                // char y = s.charAt(i);
+                // s.substring(s,e) -> s is inclusive, e->exclusive
+                String curr = s.substring(j, i);
+                if (dp[j] && wordDictSet.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
