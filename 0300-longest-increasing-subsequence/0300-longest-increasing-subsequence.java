437a0efde586ff399822a14571c91e6484b87c9d
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-09-01 20:15:14 +0530

    Time: 36 ms (78.61%), Space: 43.7 MB (97.89%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -0,0 +1,50 @@
+// class Solution {
+//     public int lengthOfLIS(int[] nums) {
+//         int n = nums.length;
+//         int[] dp = new int[n+1];
+//         Arrays.fill(dp,1);
+
+//         for(int i = 1; i < n; i++)
+//         {
+//             for(int j = 0; j < i; j++){
+//                 if(nums[i] > nums[j])
+//                 {
+//                     dp[i] = Math.max(dp[i], dp[j]+1);
+//                 }
+//             }
+//         }
+
+//         int longest = 0;
+//         for(int x : dp)
+//         {
+//             longest = Math.max(longest, x);
+//         }
+
+//         return longest; 
+//     }
+// }
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] LIS = new int[n];
+
+        LIS[0] = 1;
+        for(int i = 1; i < n; i++){
+            LIS[i] = 1;
+            for(int j = 0; j < i; j++){
+                if(nums[j] < nums[i]){
+                    LIS[i] = Math.max(LIS[i], LIS[j]+1);
+                }
+            }
+        }
+
+        int ans = LIS[0];
+
+        for(int i = 0; i < n; i++){
+            ans = Math.max(ans,LIS[i]);
+        }
+
+        return ans;
+    }
+}
