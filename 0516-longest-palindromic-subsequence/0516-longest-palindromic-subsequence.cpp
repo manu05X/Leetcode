7f4972b72094b6a1ae0d5179a4e9463e74462d2b
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-09-11 00:25:21 +0530

    Time: 58 ms (69.79%), Space: 74.5 MB (69.36%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -6,11 +6,13 @@ public:
         vector<vector<int>> dp(n, vector<int>(n));
 
         for(int gap = 0; gap < n; gap++){
+            //every word start from 0 go till gap
             for(int i = 0, j = gap; j < n; i++, j++){
                 if(gap == 0){
                     dp[i][j] = 1;
                 }
                 else if(gap == 1){
+                    //if extreme char is same then put 2 else 1
                     dp[i][j] = s[i] == s[j]? 2:1;
                 } else {
                     //if extream char is same
