bef690958dba33095ac45da6215a1924d1a26391
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-09-11 15:57:36 +0530

    Time: 8 ms (16.85%), Space: 44.5 MB (19.59%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,13 +1,33 @@
- class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {        
-        for (int row = triangle.size() - 2; row >= 0; row--) {
-            for (int col = 0; col <= row; col++) {
-                int bestBelow = Math.min(
-                    triangle.get(row + 1).get(col), 
-                    triangle.get(row + 1).get(col + 1));
-                triangle.get(row).set(col, bestBelow + triangle.get(row).get(col));
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+
+        for(int r = 1; r < n; r++){
+            for(int c = 0; c <= r; c++){
+                int smallestAbove = Integer.MAX_VALUE;
+
+                if(c > 0){ // this cover the case of col == row
+                    smallestAbove = triangle.get(r-1).get(c-1);
+                }
+
+                if(c < r){
+                    smallestAbove = Math.min(smallestAbove, triangle.get(r-1).get(c));
+                }
+
+                int path = smallestAbove + triangle.get(r).get(c);
+
+                triangle.get(r).set(c,path);
             }
         }
-        return triangle.get(0).get(0);
+
+        int minVal = triangle.get(n-1).get(0);
+
+        for(int i = 0; i < triangle.get(n-1).size(); i++){
+            if(triangle.get(n-1).get(i) < minVal){
+                minVal = triangle.get(n-1).get(i);
+            }
+        }
+
+        return minVal;
     }
 }
