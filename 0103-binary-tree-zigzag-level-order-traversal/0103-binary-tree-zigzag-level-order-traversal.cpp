df585d79f0652f14a240afa6404f108ae504a598
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-09-13 09:45:25 +0530

    Time: 3 ms (51.51%), Space: 13.3 MB (56.71%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -11,37 +11,55 @@
  */
 class Solution {
 public:
+    // Function to perform zigzag level order traversal of a binary tree
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        int c = 1;
-        vector<vector<int>> ans;
-        if(!root){
+        int c = 1; // Counter to keep track of the level (odd/even) for zigzag traversal
+        vector<vector<int>> ans; // Resultant vector to store the zigzag level order traversal
+        
+        // If the tree is empty, return the empty result
+        if (!root) {
             return ans;
         }
+        
+        // Queue to perform level order traversal (Breadth-First Search, BFS)
         queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty())
-        {
-            vector<int> temp;
-            int n = q.size();
-            for(int i=0; i<n; i++)
-            {
-                TreeNode* curr = q.front();
-                temp.push_back(curr->val);
-                if(curr->left){
+        q.push(root); // Start with the root node
+        
+        // While there are nodes to process in the queue
+        while (!q.empty()) {
+            vector<int> temp; // Temporary vector to store nodes at the current level
+            int n = q.size(); // Number of nodes at the current level
+            
+            // Traverse all nodes at the current level
+            for (int i = 0; i < n; i++) {
+                TreeNode* curr = q.front(); // Get the front node from the queue
+                temp.push_back(curr->val);  // Add the node's value to the temporary vector
+                
+                // Push left child to the queue if it exists
+                if (curr->left) {
                     q.push(curr->left);
                 }
-                if(curr->right){
+                
+                // Push right child to the queue if it exists
+                if (curr->right) {
                     q.push(curr->right);
                 }
-                q.pop();
+                
+                q.pop(); // Remove the processed node from the queue
             }
-            if(c%2 == 0)
-            {
-                reverse(temp.begin() , temp.end());
+            
+            // If the current level is even (c % 2 == 0), reverse the order of nodes for zigzag effect
+            if (c % 2 == 0) {
+                reverse(temp.begin(), temp.end());
             }
-            c++;
+            
+            c++; // Increment level counter for the next level
+            
+            // Add the current level nodes (temp) to the result (ans)
             ans.push_back(temp);
         }
+        
+        // Return the result containing zigzag level order traversal
         return ans;
     }
 };
