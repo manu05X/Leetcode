00fcd48540d3bbb6e4be53e66b7cad1094275571
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-09-13 10:17:17 +0530

    Time: 5 ms (76.3%), Space: 20 MB (45.38%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -11,14 +11,21 @@
  */
 class Solution {
 public:
+    // Function to determine if there's a root-to-leaf path in the binary tree where the sum of the node values equals the targetSum
     bool hasPathSum(TreeNode* root, int targetSum) {
+        // Base case: if the current node (root) is null, there's no path, so return false
         if(root == NULL)
             return false;
+        
+        // Subtract the value of the current node from the targetSum
         targetSum -= root->val;
+        
+        // Check if the current node is a leaf node (no left or right children)
+        // If it is a leaf, check if the targetSum is now 0
         if((root->left == NULL) && (root->right == NULL))
-            return (targetSum == 0);
+            return (targetSum == 0);  // Return true if the targetSum equals 0, meaning the path sum matches
         
+        // Recurse to the left and right subtrees, checking if either of them has a path sum that equals the updated targetSum
         return hasPathSum(root->left, targetSum) || hasPathSum(root->right, targetSum);
-        
     }
 };
