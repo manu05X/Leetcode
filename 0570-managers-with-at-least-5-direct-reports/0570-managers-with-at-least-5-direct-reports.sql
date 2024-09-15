05598ce933c900e5c17817994b4402d481070697
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-09-15 19:46:58 +0530

    Time: 278 ms (99.39%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -2,6 +2,10 @@
 
 -- SELECT name ,COUNT(managerId)>= 5 FROM Employee
 
+-- The HAVING clause was added to SQL because the WHERE keyword cannot be used with aggregate functions.
+
+-- The GROUP BY statement is often used with aggregate functions (COUNT(), MAX(), MIN(), SUM(), AVG()) to group the result-set by one or more columns.
+
 -- SELECT 
 --     managerId FROM Employee 
 -- GROUP BY 
