e00496e56a8a707dbe1d0c7c66c94bf9ed765aa4
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-10-02 09:57:35 +0530

    Time: 651 ms (5.05%), Space: 44.6 MB (77.79%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -25,6 +25,29 @@
 //[1, 4]
 
 
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         int n = nums.length;
+//         Map<Integer, Integer> mp=new HashMap<>();
+//         //int ans[] = new int[2];
+
+//         for(int i=0; i<n;i++)
+//         {
+//           int difference=target-nums[i]; //9-2=7
+//           if(mp.containsKey(difference) == true)
+//           {
+//             //   ans[0] = mp.get(difference); // 4->1; mp.get(4) = 1 // map wala index
+//             //   ans[1] = i; // current index
+//             return new int[]{mp.get(difference), i};
+//           }
+//           //      k     V
+//           mp.put(nums[i],i);
+//         }
+//         return new int[]{};
+//     }
+// }
+
+// using entrySet of map
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int n = nums.length;
@@ -34,12 +57,13 @@ class Solution {
         for(int i=0; i<n;i++)
         {
           int difference=target-nums[i]; //9-2=7
-          if(mp.containsKey(difference) == true)
-          {
-            //   ans[0] = mp.get(difference); // 4->1; mp.get(4) = 1 // map wala index
-            //   ans[1] = i; // current index
-            return new int[]{mp.get(difference), i};
-          }
+          
+          // Using entrySet to iterate over the map
+            for (Map.Entry<Integer, Integer> entry : mp.entrySet()) {
+                if (entry.getKey() == difference) {
+                    return new int[] { entry.getValue(), i };
+                }
+            }
           //      k     V
           mp.put(nums[i],i);
         }
