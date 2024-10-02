8d0decc0fcdf3e5229563b8b22be91fd20ca14df
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-10-02 09:52:53 +0530

    Time: 2 ms (98.71%), Space: 45 MB (31.73%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,24 +1,48 @@
+//6.1.24
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         int n = nums.length;
+//         Map<Integer, Integer> mp=new HashMap<>();
+//         int ans[] = new int[2];
+
+//         for(int i=0; i<n;i++)
+//         {
+//           int difference=target-nums[i]; //9-2=7
+//           if(mp.containsKey(difference) == true)
+//           {
+//               ans[0] = mp.get(difference); // 4->1; mp.get(4) = 1 // map wala index
+//               ans[1] = i; // current index
+//             return ans;
+//           }
+//           //      k     V
+//           mp.put(nums[i],i);
+//         }
+//         return ans;
+//     }
+// }
+
+//[3,4,9,6,4] , T = 8
+//[1, 4]
+
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int n = nums.length;
         Map<Integer, Integer> mp=new HashMap<>();
-        int ans[] = new int[2];
+        //int ans[] = new int[2];
 
         for(int i=0; i<n;i++)
         {
           int difference=target-nums[i]; //9-2=7
           if(mp.containsKey(difference) == true)
           {
-              ans[0] = mp.get(difference); // 4->1; mp.get(4) = 1 // map wala index
-              ans[1] = i; // current index
-            return ans;
+            //   ans[0] = mp.get(difference); // 4->1; mp.get(4) = 1 // map wala index
+            //   ans[1] = i; // current index
+            return new int[]{mp.get(difference), i};
           }
           //      k     V
           mp.put(nums[i],i);
         }
-        return ans;
+        return new int[]{};
     }
 }
-
-//[3,4,9,6,4] , T = 8
-//[1, 4]
