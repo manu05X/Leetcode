f65c360c9be515d516333c545c446d55d0cd141c
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-10-04 16:02:10 +0530

    Time: 37 ms (72.15%), Space: 35.3 MB (79.91%) - LeetHub

---
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        // Create a hash map to store the frequency of each prefix sum mod k.
+        unordered_map<int, int> prefixSumCount {{0, 1}};
+        int countSubarrays = 0; // Initialize the number of subarrays divisible by k.
+        int cumulativeSum = 0; // This will keep track of the cumulative sum of elements.
+
+        // Iterate over each number in the array.
+        for (int num : nums) {
+            // Add current number to cumulative sum and do mod by k.
+            // The double mod ensures that cumulativeSum is positive.
+            cumulativeSum = ((cumulativeSum + num) % k + k) % k;
+
+            // If a subarray has cumulativeSum mod k equals to some previous subarray's cumulativeSum mod k,
+            // then the subarray in between is divisible by k.
+            countSubarrays += prefixSumCount[cumulativeSum]++;
+
+            // The prefixSumCount[cumulativeSum]++ increases the count of the
+            // current prefix sum mod k, which will be used for future subarray checks.
+        }
+
+        // Return the total count of subarrays with sum divisible by k.
+        return countSubarrays;
+    }
+};
