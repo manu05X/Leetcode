1cab6257928f50cb2543e7073d15b877df314079
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-10-04 08:47:58 +0530

    Time: 18 ms (5.82%), Space: 19.5 MB (30.27%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,5 +1,40 @@
 class Solution {
 public:
+// A function that reverses characters from startRev to endRev in place
+void StrRev(string &str, int startRev, int endRev) {
+    while (startRev < endRev) {
+        char temp = str[startRev];
+        str[startRev] = str[endRev];
+        str[endRev] = temp;
+        startRev++;
+        endRev--;
+    }
+}
+    string reverseWords(string s) 
+    {
+        s = regex_replace(s, regex("^ +| +$|( ) +"), "$1");
+
+        int strLen = s.length() - 1;
+    
+        StrRev(s, 0, strLen);
+
+        for (int start = 0, end = 0; end <= strLen; ++end) {
+            if (end == strLen || s[end] == ' ') {
+                int endIdx = (end == strLen) ? end : end - 1;
+                StrRev(s, start, endIdx);
+                start = end + 1;
+            }
+        }
+
+        return s;
+    }
+};
+
+
+
+/*
+class Solution {
+public:
     string reverseWords(string s) {
         // Use a stringstream to extract words
         stringstream ss(s);
@@ -23,3 +58,4 @@ public:
         return result;
     }
 };
+*/
