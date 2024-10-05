366f294457ed7a9d62bc1121c5fe77302c3f205a
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-10-05 17:23:21 +0530

    Time: 40 ms (67.86%), Space: 58.9 MB (84.5%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,15 +1,37 @@
+// class Solution {
+// public:
+//     int longestSubarray(vector<int>& nums) {
+//         int zeroCount = 0; // Number of zero's in the window.
+//         int longestWindow = 0;
+
+//         int l = 0;
+//         for(int r = 0; r < nums.size(); r++){
+//             zeroCount += (nums[r] == 0);
+//             // Shrink the window until the zero counts come under the limit.
+//             while(zeroCount > 1){
+//                 zeroCount -= (nums[l] == 0);
+//                 l++;
+//             }
+
+//             longestWindow = max(longestWindow, r-l);
+//         }
+
+//         return longestWindow;
+//     }
+// };
+
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        int zeroCount = 0; // Number of zero's in the window.
+        int zeroCount = 1; // Number of zero's to delete.
         int longestWindow = 0;
 
         int l = 0;
         for(int r = 0; r < nums.size(); r++){
-            zeroCount += (nums[r] == 0);
+            zeroCount -= (nums[r] == 0);
             // Shrink the window until the zero counts come under the limit.
-            while(zeroCount > 1){
-                zeroCount -= (nums[l] == 0);
+            while(zeroCount < 0){
+                zeroCount += (nums[l] == 0);
                 l++;
             }
 
