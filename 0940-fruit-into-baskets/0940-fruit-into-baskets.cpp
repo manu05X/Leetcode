d972e4b05373b035f4dcd6015da24c00a13f38d3
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-10-05 16:26:16 +0530

    Time: 126 ms (46.83%), Space: 75 MB (69.36%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -1,33 +1,33 @@
-// class Solution {
-// public:
-//     int totalFruit(vector<int>& fruits) {
-//         int left = 0;
-//         // We use a hash map 'basket' to store the number of each type of fruit.
-//         unordered_map<int,int> mp;
-//         int maxPick = 0;
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int left = 0;
+        // We use a hash map 'basket' to store the number of each type of fruit.
+        unordered_map<int,int> mp;
+        int maxPick = 0;
 
-//         // Add fruit from the right index (right) of the window.
-//         for(int right = 0; right < fruits.size(); right++){
-//             mp[fruits[right]]++;
+        // Add fruit from the right index (right) of the window.
+        for(int right = 0; right < fruits.size(); right++){
+            mp[fruits[right]]++;
 
-//             // If the current window has more than 2 types of fruit,
-//             // we remove fruit from the left index (left) of the window,
-//             // until the window has only 2 types of fruit.
-//             while(mp.size()>2){
-//                 mp[fruits[left]]--;
+            // If the current window has more than 2 types of fruit,
+            // we remove fruit from the left index (left) of the window,
+            // until the window has only 2 types of fruit.
+            while(mp.size()>2){
+                mp[fruits[left]]--;
 
-//                 if(mp[fruits[left]] == 0){
-//                     mp.erase(fruits[left]);
-//                 }
-//                 left++;// increase the start ptr
-//             }
-//             //currPick 
-//             int currPick = right-left+1;
-//             maxPick = max(currPick,maxPick);// maxPick
-//         }
-//         return maxPick;
-//     }
-// };
+                if(mp[fruits[left]] == 0){
+                    mp.erase(fruits[left]);
+                }
+                left++;// increase the start ptr
+            }
+            //currPick 
+            int currPick = right-left+1;
+            maxPick = max(currPick,maxPick);// maxPick
+        }
+        return maxPick;
+    }
+};
 
 /*
 //  Doesn't respect the requirement of consecutive picking from left to right
@@ -65,6 +65,7 @@ public:
 };
 */
 
+/*
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
@@ -123,3 +124,4 @@ public:
         return maxFruits;
     }
 };
+*/
