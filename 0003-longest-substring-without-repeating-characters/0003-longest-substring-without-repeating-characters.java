03d222e1e09480b8dd6a507e13164bbaf6c86917
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-10-05 13:07:30 +0530

    Time: 5 ms (87.59%), Space: 44.5 MB (68.38%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -65,3 +65,20 @@ class Solution {
         return maxLength;
     }
 }
+
+/*
+abcabcbb
+01234567
+
+currDist = (r-l+1)
+
+l = 0, r = 0, currDist = 1, ans = 1, mp{a:1}
+l = 0, r = 1, currDist = 2, ans = 1, mp{a:1, b:2}
+l = 0, r = 2, currDist = 3, ans = 3, mp{a:1, b:2, c:3}
+l = 1, r = 3, currDist = 3, ans = 3, mp{a:4, b:2, c:3}, as a is repeting so change the left to where it was previously found i.e 1
+l = 2, r = 4, currDist = 3, ans = 3, mp{a:4, b:5, c:3} similarly for b=5, l = 2
+l = 3, r = 5, currDist = 3, ans = 3, mp{a:4, b:5, c:6} similarly for c=6, l = 3
+l = 5, r = 6, currDist = 2, ans = 3, mp{a:4, b:6, c:6} similarly for b=6, l = 5
+l = 7, r = 7, currDist = 1, ans = 3, mp{a:4, b:7, c:6} similarly for b=6, l = 7
+
+*/
