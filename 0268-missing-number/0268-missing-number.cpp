000a4b18abebb31a902fc1175142ff215f5a95b0
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-10-06 12:35:35 +0530

    Time: 7 ms (96.67%), Space: 20.8 MB (27.98%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& array) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
+        int n = array.size();
+        // Initialize 'missing' to the size of the vector, as the array is
+        // supposed to contain all numbers from 0 to n, so 'n' is the initial candidate.
+        int missing = n;
+        
+        for(int i = 0; i < n; i++){
+            /*XOR 'missing' with the current index 'i' and the element at that index 'nums[i]'.
+            The idea behind this is that when a number and its index are XOR'ed
+            in the range from 0 to n, the only number that will not be cancelled
+            out is the missing number.*/
+            missing ^=i^array[i];
+        }
+        return missing;
+    }
+};
