a9ad4b4d7c6b33b03530091a4dd188a625e0e069
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-10-06 09:51:03 +0530

    Time: 8 ms (64.7%), Space: 46.2 MB (28.73%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -1,21 +1,46 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        int[] starts = new int[intervals.length];
-        int[] ends = new int[intervals.length];
-        for (int i = 0; i < intervals.length; i++) {
-            starts[i] = intervals[i][0];
-            ends[i] = intervals[i][1];
-        }
-        Arrays.sort(starts);
-        Arrays.sort(ends);
-        int rooms = 0, endsItr = 0;
-        for (int i = 0; i < starts.length; i++) {
-            if (starts[i] < ends[endsItr]) {
-                rooms++;
-            } else {
-                endsItr++;
+        int n = intervals.length;
+        Arrays.sort(intervals, (a,b)->Integer.compare(a[0],b[0]));
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        minHeap.add(intervals[0][1]);
+
+        for(int i = 1; i < n; i++){
+            if(intervals[i][0] >= minHeap.peek()){
+                minHeap.poll();
             }
+
+            minHeap.add(intervals[i][1]);
         }
-        return rooms;
+
+        return minHeap.size();
     }
 }
+
+
+
+
+
+// class Solution {
+//     public int minMeetingRooms(int[][] intervals) {
+//         int[] starts = new int[intervals.length];
+//         int[] ends = new int[intervals.length];
+//         for (int i = 0; i < intervals.length; i++) {
+//             starts[i] = intervals[i][0];
+//             ends[i] = intervals[i][1];
+//         }
+//         Arrays.sort(starts);
+//         Arrays.sort(ends);
+//         int rooms = 0, endsItr = 0;
+//         for (int i = 0; i < starts.length; i++) {
+//             if (starts[i] < ends[endsItr]) {
+//                 rooms++;
+//             } else {
+//                 endsItr++;
+//             }
+//         }
+//         return rooms;
+//     }
+// }
