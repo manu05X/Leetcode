b30bd14e74bdfcfa28e5b21454df2d9d6fc97ddf
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-10-06 09:53:10 +0530

    Time: 8 ms (64.7%), Space: 45.7 MB (87.97%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -1,20 +1,25 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
         int n = intervals.length;
+        // Sort the meetings by start time
         Arrays.sort(intervals, (a,b)->Integer.compare(a[0],b[0]));
-
+        // Min-heap to track the end time of meetings
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-
+        // Add the end time of the first meeting
         minHeap.add(intervals[0][1]);
 
+        // Iterate over the rest of the meetings
         for(int i = 1; i < n; i++){
+            // If the room due to free up the earliest is free, remove it from the heap
             if(intervals[i][0] >= minHeap.peek()){
                 minHeap.poll();
             }
 
+            // Add the current meeting's end time to the heap
             minHeap.add(intervals[i][1]);
         }
 
+        // The size of the heap is the number of rooms needed
         return minHeap.size();
     }
 }
