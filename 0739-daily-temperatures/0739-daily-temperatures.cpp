09128fe92c62e6ada267555e86463c1336f250fc
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-10-07 16:54:51 +0530

    Time: 65 ms (99.84%), Space: 102.5 MB (69.11%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,29 +1,60 @@
+/*
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& temperatures) {
+    vector<int> dailyTemperatures(vector<int>& t) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         cout.tie(NULL);
-        
-        int n = temperatures.size(); // Number of days based on the temperature list
-        vector<int> daysToWait(n); // Initialize the answer array with the same size as temperatures
-        stack<int> indexStack; // Stack to keep track of temperatures indices
 
-        // Iterate over each day in temperatures
-        for (int i = 0; i < n; ++i) {
-            // Check if the current day's temperature is higher than the temperature at the 
-            // top of the stack (which represents the last unprocessed day's temperature)
-            while (!indexStack.empty() && temperatures[indexStack.top()] < temperatures[i]) {
-                int previousDayIndex = indexStack.top(); // Get the index of the day with the lower temperature
-                daysToWait[previousDayIndex] = i - previousDayIndex; // Calculate the days to wait
-                indexStack.pop(); // Remove that day from the stack since it's now processed
+        int n = t.size();
+        vector<int> ans(n, 0);
+
+        for (int currDay = n - 2; currDay >= 0; currDay--) {
+            int futureDay = currDay + 1;
+
+            while (futureDay < n && t[currDay] >= t[futureDay]) {
+                if (ans[futureDay] == 0) {
+                    futureDay = n;
+                    break;
+                }
+                futureDay += ans[futureDay];
             }
-          
-            // Always push the current day's index to the stack to process later
-            indexStack.push(i);
+
+            if (futureDay < n) {
+                ans[currDay] = futureDay - currDay;
+            }
         }
-      
-        // The stack will automatically contain 0s where there is no warmer temperature in the future
-        return daysToWait;
+
+        return ans;
+    }
+};
+*/
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& t) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+
+        int n = t.size();
+        vector<int> ans(n, 0);
+
+        int* st = new int[n];
+        int top = -1;
+
+        for (int currDay = 0; currDay < n; currDay++) 
+        {
+            while (top >= 0 && t[currDay] > t[st[top]]) {
+                int pastIdx = st[top];
+                ans[pastIdx] = currDay - pastIdx;
+                top--;
+            }
+
+            top++;
+            st[top] = currDay;
+
+        }
+
+        return ans;
     }
 };
