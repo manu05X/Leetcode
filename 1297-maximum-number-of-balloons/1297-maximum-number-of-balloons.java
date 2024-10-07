e72a0b537a43f2b5961f92ca6c04c18791f616c1
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-10-07 19:06:43 +0530

    Time: 1 ms (100%), Space: 41.9 MB (77.59%) - LeetHub

--- 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.java
@@ -1,5 +1,27 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
+        int b = 0, a = 0, l = 0, o = 0, n = 0;
+        for (char aChar : text.toCharArray())
+            switch (aChar) {
+                case 'b' : b++;
+                break;
+                case 'a' : a++;
+                break;
+                case 'l' : l++;
+                break;
+                case 'o' : o++;
+                break;
+                case 'n' : n++;
+                break;
+            }
+        return Math.min(Math.min(Math.min(b,a),n), Math.min(l/2,o/2));
+        
+    }
+}
+
+/*
+class Solution {
+    public int maxNumberOfBalloons(String text) {
         int n = text.length();
         
         Map<Character, Integer> mp = new HashMap<>();
@@ -27,3 +49,4 @@ class Solution {
         return ans;
     }
 }
+*/
