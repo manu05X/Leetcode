24749b3a8fdb2ea6d0105eb521ba5029febed38b
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-10-14 16:08:56 +0530

    Time: 2 ms (53.33%), Space: 43.4 MB (30.94%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -21,3 +21,24 @@ class Solution {
         return ans;
     }
 }
+
+/*
+
+Algorithm (DFS-Based Approach for Generating Parentheses):
+Base Case:
+    If the length of the string s equals 2 * n (meaning the string contains exactly n pairs of parentheses), add the string to the result list (ans) and return. This is the stopping condition.
+
+Recursive Cases:
+    The algorithm continues to explore different possibilities based on the count of open and close parentheses added so far.
+    Add Open Parenthesis:
+
+        If the number of open parentheses (open) is less than n, we can add another open parenthesis '(' to the string. This corresponds to the condition if (open < n). The recursive call then continues with the updated string s and incremented open count.
+    Add Close Parenthesis:
+        If the number of close parentheses (close) is less than the number of open parentheses (open), we can add a close parenthesis ')'. This ensures that no invalid combination is formed (i.e., it prevents adding more closing brackets than opening brackets). This corresponds to the condition if (open > close). The recursive call continues with the updated string s and incremented close count.
+
+Recursive Depth:
+    The recursive process continues until the string reaches a length of 2 * n (i.e., it contains exactly n open and n close parentheses). At this point, the valid combination is added to the result list.
+
+Result:
+    After all recursive calls are completed, the list ans will contain all possible valid combinations of n pairs of balanced parentheses.
+*/
