d6ec1bf05b40b9d2a4b5b9233195584773d8911a
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-10-15 14:11:58 +0530

    Time: 0 ms (100%), Space: 56.5 MB (22.01%) - LeetHub

---
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int n = arr.length;
+
+        int lo = 0;
+        int hi = n-1;
+
+        while(lo < hi){
+            int mid = lo + (hi - lo)/2;
+
+            if(arr[mid] < arr[mid+1]){
+                lo = mid+1;
+            } else{
+                hi = mid;
+            }
+        }
+
+        return lo;
+    }
+}
+
+/*
+basically we are trying to get the leftmost occurance where mid < mid+1 is violated for the arr element
+
+*/
