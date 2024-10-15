6a59ba19638ae6227274d8895dcdff36cd68b3c1
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-10-15 19:16:47 +0530

    Time: 4 ms (23.92%), Space: 12.9 MB (47.96%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -14,25 +14,34 @@ public:
         // Binary search to find the pivot, the smallest element
         while (lo < hi) {
             int mid = lo + (hi - lo) / 2; 
-
+            /*
+            If nums[0] <= nums[mid], this means that the first half of the array (from nums[0] to nums[mid]) is sorted, so the minimum element must be in the right half. Therefore, we move lo to mid + 1.
+            */
             // if (nums[0] <= nums[mid]) {
             //     lo = mid + 1;
-            // } 
+            // }
+            /*
+            If nums[0] > nums[mid], it means the pivot is in the left half (from lo to mid), so we move hi to mid.
+            */
             // else 
             // { 
             //     hi = mid;
             // }
             /*
-            If nums[0] <= nums[mid], this means that the first half of the array (from nums[0] to nums[mid]) is sorted, so the minimum element must be in the right half. Therefore, we move lo to mid + 1.
+            nums[mid] < nums[hi] this means that the first half of the array (from nums[0] to nums[mid]) is sorted,
+            so the minimum element must be in the left half. Therefore, we move hi to mid.
             */
             if (nums[mid] < nums[hi]) 
             {
+                /*
+                If nums[mid] < nums[hi], the right half (from mid+1 to hi) is sorted, and the pivot (the smallest element) must be in the left half or at mid. Therefore, the search space is reduced by setting hi = mid.
+                */
                 hi = mid;
-            } 
-            else
+            }
             /*
-            If nums[0] > nums[mid], it means the pivot is in the left half (from lo to mid), so we move hi to mid.
+            If nums[mid] >= nums[hi], it means the left half is sorted, and the smallest element is in the right half. So, we move the lo pointer to mid + 1 to continue searching in the right half.
             */
+            else
             {
                 lo = mid+1;
             }
