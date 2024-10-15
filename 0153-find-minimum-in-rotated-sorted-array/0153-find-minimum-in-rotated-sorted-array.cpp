9fab6ccda83713ffd60725df56c5ea1db398c269
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-10-15 18:41:21 +0530

    Time: 7 ms (17.49%), Space: 12.8 MB (81.21%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -22,11 +22,17 @@ public:
             // { 
             //     hi = mid;
             // }
+            /*
+            If nums[0] <= nums[mid], this means that the first half of the array (from nums[0] to nums[mid]) is sorted, so the minimum element must be in the right half. Therefore, we move lo to mid + 1.
+            */
             if (nums[mid] < nums[hi]) 
             {
                 hi = mid;
             } 
-            else 
+            else
+            /*
+            If nums[0] > nums[mid], it means the pivot is in the left half (from lo to mid), so we move hi to mid.
+            */
             {
                 lo = mid+1;
             }
@@ -40,7 +46,9 @@ public:
 0,1,2,3,4
 3,4,5,1,2
 
-lo = 0, hi = 4, mid = 2
+lo = 0, hi = 4, mid = 2, 5 < 2 , so hi = 2
+lo = 0, hi = 2, mid = 1, 4 < 2 X, so lo = 2
+lo = 2, hi = 2, mid = 2, 5 < 2 X, so lo = 2+1 = 3
 
 
 
