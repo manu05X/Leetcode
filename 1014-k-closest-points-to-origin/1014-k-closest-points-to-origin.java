5b41bf32e255424f1b63c744cf2db854f04b7a20
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-11-02 19:40:34 +0530

    Time: 21 ms (88.49%), Space: 56.8 MB (5.87%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
@@ -1,5 +1,22 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
+        // Sort the array with a custom lambda comparator function
+        Arrays.sort(points, (a, b) -> squaredDistance(a) - squaredDistance(b));
+        
+        // Return the first k elements of the sorted array
+        return Arrays.copyOf(points, k);
+    }
+    
+    private int squaredDistance(int[] point) {
+        // Calculate and return the squared Euclidean distance
+        return point[0] * point[0] + point[1] * point[1];
+    }
+};
+
+
+/*
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
         PriorityQueue<int[]> maxHeap = new PriorityQueue<int[]>((a,b) -> (a[0]*a[0] + a[1]*a[1]) - (b[0]*b[0] + b[1]*b[1]));
 
         int[][] ans = new int[k][2];
@@ -15,3 +32,4 @@ class Solution {
         return ans;
     }
 }
+*/
