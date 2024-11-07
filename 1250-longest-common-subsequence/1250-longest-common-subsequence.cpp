ea2ec664d33c0c13fed193723aa74fd0a0d4deba
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-11-07 08:22:30 +0530

    Time: 11 ms (99.19%), Space: 7.9 MB (98.55%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -4,6 +4,39 @@ public:
         int n = text1.length();
         int m = text2.length();
 
+         vector<int> dp(m + 1, 0);
+        // i and j represent the lengths of s1 and s2 respectively
+        for(int i = 1; i <= n; i++){
+            // prev stores the value from the previous 
+            // row and previous column (i-1), (j -1)
+            int prev = dp[0];  
+            for(int j = 1; j <= m; j++){
+                // temp temporarily stores the current dp[j] before it gets updated
+                int temp = dp[j];
+                if(text1[i-1] == text2[j-1])
+                {
+                    dp[j] = 1+prev;
+                }
+                else{
+                    // Otherwise, take the maximum of the left and top values
+                    dp[j] = max(dp[j], dp[j-1]);
+                }
+                prev = temp;
+            }
+        }
+
+        return dp[m];
+    }
+};
+
+
+/*
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.length();
+        int m = text2.length();
+
          vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
@@ -20,3 +53,4 @@ public:
         return dp[n][m];
     }
 };
+*/
