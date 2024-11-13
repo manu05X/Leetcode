1c8e7238e68937f325da3d4d869eed9ed300675d
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-11-13 12:00:48 +0530

    Time: 7 ms (100%), Space: 49.1 MB (77.18%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -1,17 +1,36 @@
 class NumArray {
-    public int[] arr;
+    public int[] sum;
     public NumArray(int[] nums) {
-        arr = nums;
+        int n = nums.length;
+        sum = new int[n+1];
+
+        for(int i = 0; i < n; i++){
+            sum[i+1] = sum[i]+nums[i];
+        }
     }
     
     public int sumRange(int left, int right) {
-        int sum = 0;
-        for(int i = left; i <= right; i++){
-            sum+= arr[i];
-        }
-        return sum;
+        
+        return sum[right+1] - sum[left];
     }
 }
+
+
+
+// class NumArray {
+//     public int[] arr;
+//     public NumArray(int[] nums) {
+//         arr = nums;
+//     }
+    
+//     public int sumRange(int left, int right) {
+//         int sum = 0;
+//         for(int i = left; i <= right; i++){
+//             sum+= arr[i];
+//         }
+//         return sum;
+//     }
+// }
 
 /**
  * Your NumArray object will be instantiated and called as such:
