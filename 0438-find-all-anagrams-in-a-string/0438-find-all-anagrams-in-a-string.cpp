0c9bd2d203d3a3644b5ea51bae551d1e7ac9ac42
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-11-14 18:54:27 +0530

    Time: 6 ms (66.21%), Space: 10.1 MB (94.69%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -14,6 +14,53 @@ public:
 
         for(int i = 0; i < np; i++){
             pCharCount[p[i]-'a']++;
+            windowCharCount[s[i]-'a']++;
+        }
+
+        if (pCharCount == windowCharCount) {
+                ans.push_back(0);
+            }
+
+
+        // Slide the window over string s and compare with character counts of p.
+        for (int i = np; i < ns; ++i) {
+            windowCharCount[s[i] - 'a']++; // Add the current character to the window count.
+
+            // since we added one element to the right, 
+            // one element to the left should be discarded.
+            windowCharCount[s[i-np] - 'a']--;
+          
+            // If the window has the same character counts as p, it's an anagram starting at (i - pLength + 1).
+            if (pCharCount == windowCharCount) {
+                ans.push_back(i - np + 1);
+            }
+          
+            // // Move the window forward by one: decrease the count of the character leaving the window.
+            // windowCharCount[s[i - np + 1] - 'a']--;
+        }
+      
+        return ans;
+    }
+};
+
+
+/*
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        int ns = s.size();
+        int np = p.size();
+        vector<int> ans;
+
+        if(ns < np){
+            return ans;
+        }
+
+        vector<int> windowCharCount(26,0);
+        vector<int> pCharCount(26,0);
+
+        for(int i = 0; i < np; i++){
+            pCharCount[p[i]-'a']++;
         }
 
         for(int i = 0; i < np-1; i++){
@@ -36,3 +83,4 @@ public:
         return ans;
     }
 };
+*/
