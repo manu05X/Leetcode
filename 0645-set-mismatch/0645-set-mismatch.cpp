b9d14e561e4eadf676fd031bb9c992bba9c4a6ce
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-11-17 14:22:46 +0530

    Time: 2 ms (62.22%), Space: 24.1 MB (79.5%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -28,3 +28,39 @@ public:
 
     }
 };
+
+/*
+
+Dry Run
+Input:
+nums = [1, 2, 2, 4]
+
+Execution:
+Initial State: nums = [1, 2, 2, 4], dup = -1, missing = 1
+
+Marking Visited Numbers:
+
+Index (i)	nums[i]	abs(nums[i]) - 1	nums Before Update	Action	nums After Update	dup
+0	1	0	[1, 2, 2, 4]	Negate nums[0]	                            [-1, 2, 2, 4]	-1
+1	2	1	[-1, 2, 2, 4]	Negate nums[1]	                            [-1, -2, 2, 4]	-1
+2	2	1	[-1, -2, 2, 4]	nums[1] < 0, set dup=2	                    [-1, -2, 2, 4]	2
+3	4	3	[-1, -2, 2, 4]	Negate nums[3]	                            [-1, -2, 2, -4]	2
+
+
+Finding Missing Number:
+Index (i)	nums[i]	Condition (nums[i] > 0)	Missing
+0	            -1	        False	        1
+1	            -2	        False	        1
+2	            2	        True	        3
+3	            -4	        False	        3
+
+
+Final Result:
+dup = 2, missing = 3
+Return [2, 3]
+
+Output:
+Duplicate: 2, Missing: 3
+Result: [2, 3]
+
+*/
