c5295ae54afbae317adc9ac935daccad2831a23c
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-11-19 10:41:42 +0530

    Time: 50 ms (42.98%), Space: 59 MB (33.65%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,6 +1,44 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
         int n = nums.length;
+        long maxSum = Integer.MIN_VALUE;
+        long currWindowSum = 0;
+
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        int left = 0;
+
+
+        for(int right = 0; right < n; right++){
+            int x = nums[right];
+            currWindowSum += x;
+            mp.put(x, mp.getOrDefault(x,0)+1);
+            int currWindowLength = right-left+1;
+
+            while(currWindowLength > k){
+                currWindowSum -= nums[left];
+                mp.put(nums[left], mp.getOrDefault(nums[left],0)-1);
+
+                if(mp.get(nums[left]) == 0){
+                    mp.remove(nums[left]);
+                }
+                left++;
+
+                currWindowLength = right-left+1;
+            }
+
+            if(k == currWindowLength && mp.size() == k){
+                maxSum = Math.max(maxSum, currWindowSum);
+            }
+        }
+
+        return maxSum == Integer.MIN_VALUE ? 0 : maxSum;
+    }
+}
+
+/*
+class Solution {
+    public long maximumSubarraySum(int[] nums, int k) {
+        int n = nums.length;
         long maxSum = 0;
         long currWindowSum = 0;
 
@@ -31,3 +69,5 @@ class Solution {
         return maxSum;
     }
 }
+
+ */
