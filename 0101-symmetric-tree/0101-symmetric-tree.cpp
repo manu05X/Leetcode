9e728762ad985f94c39cb7c19f3d2c99c1a06eb5
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-11-20 08:25:41 +0530

    Time: 0 ms (100%), Space: 17.6 MB (27.56%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -27,3 +27,31 @@ public:
         return isSymmetricUtil(root->left, root->right);
     }
 };
+
+/*
+The tree's symmetry means the left subtree of one tree should mirror the right subtree of the other tree and vice versa. For the tree to remain symmetric:
+
+The left child of root1 should match the right child of root2.
+The right child of root1 should match the left child of root2.
+
+        1
+       / \
+      2   2
+     / \ / \
+    3  4 4  3
+
+Step 1: Check if root == NULL. False, proceed.
+Step 2: Call isSymmetricUtil(root->left, root->right):
+    Compare root1 = 2 and root2 = 2. Equal, proceed.
+    Call isSymmetricUtil(root1->left, root2->right):
+        Compare root1 = 3 and root2 = 3. Equal, proceed.
+        Call isSymmetricUtil(NULL, NULL) twice. Both return true.
+    Call isSymmetricUtil(root1->right, root2->left):
+        Compare root1 = 4 and root2 = 4. Equal, proceed.
+        Call isSymmetricUtil(NULL, NULL) twice. Both return true.
+    Result for the current subtree is true.
+Step 3: Similarly, process the mirrored subtrees:
+    isSymmetricUtil(root1->right, root2->left) (left = 4, right = 4) -> true.
+Final Result: The tree is symmetric.
+
+*/
