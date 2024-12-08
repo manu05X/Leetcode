1520fbe8656be71b2f2b431d6317a81b38ed294a
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-12-08 10:43:22 +0530

    Time: 8 ms (5.45%), Space: 22.2 MB (5.47%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -7,13 +7,20 @@ public:
             bool flag = true;
 
             while(!st.empty() && (st.top() > 0 && astorid < 0)){
+                // If the top asteroid in the stack is smaller, then it will explode.
+                // Hence pop it from the stack, also continue with the next asteroid in the stack.
                 if(abs(st.top()) < abs(astorid)){
                     st.pop();
                     continue;
-                } else if(abs(st.top()) == abs(astorid)){
+                } 
+                // If both asteroids are the same size, then both asteroids will explode.
+                // Pop the asteroid from the stack; also, we won't push the current asteroid to the stack.
+                else if(abs(st.top()) == abs(astorid)){
                     st.pop();
                 }
 
+                // If we reach here, the current asteroid will be destroyed
+                // Hence, we should not add it to the stack
                 flag = false;
                 break;
             }
@@ -23,7 +30,7 @@ public:
             }
         }
 
-         // Add the asteroids from the stack to the vector in the reverse order.
+        // Add the asteroids from the stack to the vector in the reverse order.
         vector<int> ans(st.size());
         for (int i = ans.size() - 1; i >= 0; i--) {
             ans[i] = st.top();
