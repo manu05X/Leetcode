9d23fedfb5393e9ed9bbf3c61f5772047e061838
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-12-09 13:57:32 +0530

    Time: 0 ms (100%), Space: 42.3 MB (31.4%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,16 +1,22 @@
 class Solution {
     List<String> ans = new ArrayList<>();
 
-    public void dfs(String digits, String[] pad, String currString, int idx){
+    public void dfs(String digits, String[] pad, StringBuilder currString, int idx){
 
         if(idx == digits.length()){
-            ans.add(currString);
+            ans.add(currString.toString());
             return;
         }
 
-        String currPadString = pad[digits.charAt(idx)-'0'];
-        for(int i = 0; i < currPadString.length(); i++){
-            dfs(digits,pad,currString + currPadString.charAt(i),idx+1);
+        // Get the letters that the current digit maps to, and loop through them
+        // { 0,  1,  2  ,  3   ,    4  ,   5  ,   6  ,    7  ,    8 ,   9   };
+        // {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        // Get the letters that the current digit maps to, and loop through them
+        String currPadString = pad[digits.charAt(idx) - '0'];
+        for (int i = 0; i < currPadString.length(); i++) {
+            currString.append(currPadString.charAt(i));  // Append to StringBuilder
+            dfs(digits, pad, currString, idx + 1);
+            currString.deleteCharAt(currString.length() - 1);  // Backtrack
         }
     }
 
@@ -20,7 +26,7 @@ class Solution {
         }
         String[] pad = {"","","abc","def","ghi", "jkl","mno","pqrs","tuv","wxyz"};
 
-        dfs(digits,pad,"",0);
+        dfs(digits,pad,new StringBuilder(),0);
 
         return ans;
     }
