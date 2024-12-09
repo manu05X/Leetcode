fff968620579f11f3dafd2ee21f730223e2d119b
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-12-09 13:38:34 +0530

    Time: 7 ms (48.12%), Space: 13.5 MB (72.43%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
@@ -28,3 +28,36 @@ public:
         return ans;
     }
 };
+
+
+/*
+class Solution {
+public:
+    vector<vector<int>> allPaths;
+    
+    void dfs(int node, vector<int>& temPath, vector<vector<int>>& graph) {
+        // Add the current node to the path
+        temPath.push_back(node);
+
+        // If we reached the last node (node n-1), add the current path to the result
+        if (node == graph.size() - 1) {
+            allPaths.push_back(temPath);
+        } else {
+            // Explore all neighbors of the current node
+            for (int i = 0; i < graph[node].size(); i++) {
+                dfs(graph[node][i], temPath, graph);
+            }
+        }
+        
+        // Backtrack: remove the current node from the path
+        temPath.pop_back();
+    }
+    
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<int> temPath;
+        dfs(0, temPath, graph);
+        return allPaths;
+    }
+};
+
+*/
