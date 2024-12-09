cd4de5fbef3ed257cc670f6311aaa5e0f043822c
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-12-09 13:48:19 +0530

    Time: 0 ms (100%), Space: 9.2 MB (18.42%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -8,7 +8,9 @@ public:
         }
 
         // Get the letters that the current digit maps to, and loop through them
-        string currValue = pad[digits[idx]-'0'];
+        // { 0,  1,  2  ,  3   ,    4  ,   5  ,   6  ,    7  ,    8 ,   9   };
+        // {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        string currValue = pad[digits[idx]-'0']; 
         for(int i = 0; i < currValue.size(); i++){
             // Add the letter to our current path
             tmp.push_back(currValue[i]);
