086012da4517561a62c0c4cbb1b08f1a9da5e057
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-12-10 06:35:43 +0530

    Time: 7 ms (83.54%), Space: 10.9 MB (78.06%) - LeetHub

--- 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.cpp
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -2,6 +2,42 @@ class Solution {
 public:
     int maximumLength(string s) {
         int n = s.size();
+        map<pair<char, int>, int> mp;
+        int substrLength = 0;
+
+        for(int left = 0; left < n; left++){
+            char currChar = s[left];
+            substrLength = 0;
+            for(int right = left; right < n; right++){
+                // If the string is empty, or the current character is equal to the previously added character, then add it to the map.
+                if(currChar == s[right]){
+                    substrLength++;
+                    mp[{currChar, substrLength}]++;
+                } else {
+                    break;
+                }
+            }
+        }
+
+
+        int ans = 0;
+        for(auto it : mp){
+            int len = it.first.second;
+
+            if(it.second >= 3 && len > ans){
+                ans = len;
+            } 
+        }
+
+        return ans == 0 ? -1 : ans;
+    }
+};
+
+/*
+class Solution {
+public:
+    int maximumLength(string s) {
+        int n = s.size();
         map<string, int> mp;
 
         for(int left = 0; left < n; left++){
@@ -29,3 +65,4 @@ public:
         return ans == 0 ? -1 : ans;
     }
 };
+*/
