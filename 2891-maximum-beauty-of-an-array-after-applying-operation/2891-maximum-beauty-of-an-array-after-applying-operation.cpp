e48b047731abf9e3d829e6e83a09c4239a153c9f
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-12-11 11:53:34 +0530

    Time: 51 ms (60.73%), Space: 104.9 MB (94.06%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -2,17 +2,20 @@ class Solution {
 public:
     int maximumBeauty(vector<int>& nums, int k) {
         int n = nums.size();
-        sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end()); // Sort the array
 
-        int right = 0;
-        int maxBeauty = 0;
+        int right = 0; // Right pointer
+        int maxBeauty = 0; // Maximum size of the range
 
-        for(int left = 0; left < n; left++){
-            while(right < n && nums[right] - nums[left] <= 2*k){
+        // Iterate with the left pointer
+        for (int left = 0; left < n; left++) {
+            // Expand the right pointer as long as the condition is satisfied
+            while (right < n && nums[right] - nums[left] <= 2 * k) {
                 right++;
             }
 
-            maxBeauty = max(maxBeauty, right-left);
+            // Update the maximum beauty
+            maxBeauty = max(maxBeauty, right - left);
         }
 
         return maxBeauty;
