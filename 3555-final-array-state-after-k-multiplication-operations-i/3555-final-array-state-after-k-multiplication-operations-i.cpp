48b9bafc854b32f48296a181138e66c5bcacc3a9
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-12-16 06:31:40 +0530

    Time: 4 ms (20.56%), Space: 27.8 MB (20.77%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
@@ -5,17 +5,20 @@ public:
 
         priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
 
+        // Initialize the priority queue with elements and their indices
         for(int i = 0; i < n; i++){
             pq.push({nums[i], i});
         }
 
         while(k--){
+            // Get the smallest element and its index
             auto [currValue, idx] = pq.top();
             pq.pop();
 
             // Update the selected value by multiplying it with the multiplier
             nums[idx] = currValue * multiplier;
 
+            // Push the updated value back into the priority queue
             pq.push({nums[idx], idx});
         }
 
