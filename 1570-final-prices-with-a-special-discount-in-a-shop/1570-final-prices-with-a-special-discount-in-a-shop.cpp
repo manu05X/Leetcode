2c40ec6b9b99db213d3f7646e3e132859d96bd85
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-12-18 09:44:06 +0530

    Time: 0 ms (100%), Space: 14.1 MB (24.39%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -7,7 +7,7 @@ public:
 
         for(int i = 0; i < n; i++){
 
-            while(!st.empty() && prices[i] <= prices[st.top()]){
+            while(!st.empty() && prices[st.top()] >= prices[i]){
                 ans[st.top()] -= prices[i];
                 st.pop();
             }
@@ -20,14 +20,34 @@ public:
         return ans;
     }
 };
+
+
+
 /*
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        int n = prices.size();
+        stack<int> st;
+
+        for(int i = n-1; i >= 0; i--){
+
+            while(!st.empty() && prices[st.top()] > prices[i]){
+                st.pop();
+            }
+
+            if(!st.empty()){
+                prices[i]-= prices[st.top()];
+                
+
+            } else {
+                st.push(i);
+            }
+
+        }
 
 
-
-
-
-
-
-
-
+        return prices;
+    }
+};
 */
