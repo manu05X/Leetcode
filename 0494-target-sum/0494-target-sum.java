8f087dd085f04cf4e2610486896b0cbe96a0dabb
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2024-12-26 08:39:19 +0530

    Time: 9 ms (64.77%), Space: 44.6 MB (33.71%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,4 +1,49 @@
 class Solution {
+    int n = 0;
+    int totalSum = 0;
+
+    public int findTargetSumWays(int[] nums, int target) {
+        n = nums.length;
+        totalSum = Arrays.stream(nums).sum();
+        int [][] memo = new int[n][2*totalSum+1];
+
+        for(int[] row : memo){
+            Arrays.fill(row, Integer.MIN_VALUE);
+        }
+
+        return dfs(nums, 0, 0, target, memo);
+    }
+
+    private int dfs(int[] nums, int currIdx, int currSum ,int target, int[][] memo){
+        // if current index is equal to the length of nums
+        if(currIdx == n){
+            // if target is current Sum so, increase ans
+            if(currSum == target){
+                return 1;
+            } else{
+                return 0;
+            }
+        } else{
+            if(memo[currIdx][currSum+totalSum] != Integer.MIN_VALUE){
+                return memo[currIdx][currSum+totalSum];
+            }
+
+            // add the curr(currIdx) nums element to currSum, and increase the index by 1
+            int add = dfs(nums, currIdx+1, currSum+nums[currIdx], target, memo);
+            // Subbtract the curr(currIdx) nums element to currSum and increase the index by 1
+            int sub = dfs(nums, currIdx+1, currSum-nums[currIdx], target, memo);
+
+            memo[currIdx][currSum+totalSum] = add + sub;
+            return memo[currIdx][currSum+totalSum];
+        }
+    }
+}
+
+/*
+
+Time complexity: O(2^n) i.e exponential
+
+class Solution {
     int ans = 0;
     int n = 0;
     public int findTargetSumWays(int[] nums, int target) {
@@ -25,3 +70,6 @@ class Solution {
 
     }
 }
+
+
+ */
