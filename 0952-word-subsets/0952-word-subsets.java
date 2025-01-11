240d62dc5a313544100227db3d034a03aff944be
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-01-11 11:11:35 +0530

    Time: 15 ms (30.18%), Space: 54.6 MB (78.7%) - LeetHub

--- 0952-word-subsets/0952-word-subsets.java
+++ 0952-word-subsets/0952-word-subsets.java
@@ -1,6 +1,45 @@
 class Solution {
     public List<String> wordSubsets(String[] A, String[] B) {
         int[] bmax = count("");
+        for (String b : B) {
+            int[] bCount = count(b);
+            for (int i = 0; i < 26; ++i) {
+                bmax[i] = Math.max(bmax[i], bCount[i]);
+            }
+        }
+
+        List<String> ans = new ArrayList<>();
+        for (String a : A) {
+            int[] aCount = count(a);
+            boolean isUniversal = true; // Flag to check if 'a' is universal
+            for (int i = 0; i < 26; ++i) {
+                if (aCount[i] < bmax[i]) {
+                    isUniversal = false;
+                    break; // Break out of the inner loop
+                }
+            }
+            if (isUniversal) {
+                ans.add(a); // Add 'a' to the result if it satisfies all conditions
+            }
+        }
+
+        return ans;
+    }
+
+    public int[] count(String S) {
+        int[] ans = new int[26];
+        for (char c : S.toCharArray()) {
+            ans[c - 'a']++;
+        }
+        return ans;
+    }
+}
+
+
+/*
+class Solution {
+    public List<String> wordSubsets(String[] A, String[] B) {
+        int[] bmax = count("");
         for (String b: B) {
             int[] bCount = count(b);
             for (int i = 0; i < 26; ++i)
@@ -26,3 +65,4 @@ class Solution {
         return ans;
     }
 }
+*/
