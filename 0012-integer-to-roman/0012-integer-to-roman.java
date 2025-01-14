e47f16a77616e827388868d5b278dcf08f4f177b
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-01-14 11:17:29 +0530

    Time: 7 ms (18.51%), Space: 45 MB (16.2%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public String intToRoman(int num) {
         int[] base = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
@@ -13,5 +14,38 @@ class Solution {
             }
         }
         return result;
+    }
+}
+*/
+
+class Solution {
+    public String intToRoman(int num) {
+        // Map to store integer values and their corresponding Roman numeral symbols
+        Map<Integer, String> romanMap = new LinkedHashMap<>();
+        romanMap.put(1000, "M");
+        romanMap.put(900, "CM");
+        romanMap.put(500, "D");
+        romanMap.put(400, "CD");
+        romanMap.put(100, "C");
+        romanMap.put(90, "XC");
+        romanMap.put(50, "L");
+        romanMap.put(40, "XL");
+        romanMap.put(10, "X");
+        romanMap.put(9, "IX");
+        romanMap.put(5, "V");
+        romanMap.put(4, "IV");
+        romanMap.put(1, "I");
+
+        StringBuilder result = new StringBuilder(); // To build the resulting Roman numeral string
+
+        // Iterate through the map and construct the Roman numeral
+        for (Map.Entry<Integer, String> entry : romanMap.entrySet()) {
+            while (num >= entry.getKey()) { // While the number is greater than or equal to the key
+                result.append(entry.getValue()); // Append the corresponding Roman numeral
+                num -= entry.getKey(); // Decrease the number by the key value
+            }
+        }
+
+        return result.toString(); // Return the final Roman numeral string
     }
 }
