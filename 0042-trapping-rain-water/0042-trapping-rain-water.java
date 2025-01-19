ca595bf3f420640c301df04ba571e7055d77bd53
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-01-19 11:21:27 +0530

    Time: 0 ms (100%), Space: 46.1 MB (78.39%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,59 @@
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int left = 0, right = n-1;
+        int left_max = 0, right_max = 0;
+        int water = 0;
+
+        while(left < right)
+        {
+            /* If height[left] is smaller than height[right] as water trapped would be dependant on height of bar in current direction (from left to right). As soon as we find the bar at other end (right) is smaller, we start iterating in opposite direction (from right to left).*/
+            if(height[left] <= height[right]){
+                //If height[left] is greater than left_max, update left_max
+                if(height[left] >= left_max){
+                    left_max = height[left];
+                }
+                else{
+                    // add water
+                    water += left_max - height[left];
+                }
+                left++;
+            }
+            else{
+                if(height[right] >= right_max){
+                    right_max = height[right];
+                }
+                else{
+                    water += right_max - height[right];
+                }
+                right--;
+            }
+        }
+
+        return water;
+    }
+}
+/*
+class Solution {
+    public int trap(int[] height) {
+        // time : O(n)
+        // space : O(1)
+        if (height.length==0) return 0; 
+        int left = 0, right = height.length-1; 
+        int leftMax=0, rightMax=0; 
+        int ans = 0; 
+        while (left < right) {
+            if (height[left] > leftMax) leftMax = height[left]; 
+            if (height[right] > rightMax) rightMax = height[right];
+            if (leftMax < rightMax) {
+                ans += Math.max(0, leftMax-height[left]); 
+                left++; 
+            } else {
+                ans += Math.max(0, rightMax-height[right]); 
+                right--; 
+            }
+        }
+        return ans; 
+    }
+}
+ */
