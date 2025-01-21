7f9ed428eb0ba9db1f85a52a430977fb488981dc
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-01-21 09:42:28 +0530

    Time: 0 ms (100%), Space: 66.7 MB (76.92%) - LeetHub

---
+++ 2145-grid-game/2145-grid-game.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    long long gridGame(vector<vector<int>>& grid) {
+        //int n = grid.size();
+        int m = grid[0].size();
+
+        long long firstRowSum = 0;
+        for(int i = 0; i < m; i++){
+            firstRowSum += grid[0][i];
+        }
+
+        long long secondRowSum = 0;
+        long long minSum = LONG_MAX;
+        for(int i = 0; i < m; i++){
+            firstRowSum -= grid[0][i];
+
+            minSum = min(minSum, max(firstRowSum, secondRowSum));
+            secondRowSum += grid[1][i];
+        }
+
+        return minSum;
+    }
+};
+
+
+/*
+[2,5,4],
+[1,5,1]
+
+
+i = 0
+firstRowSum = 11-2 -> 9
+
+minSum = min(minSum, max(10,0)) => min(minSum, 9) => 9
+SecondRowSum = 0+1=>1
+
+
+i = 1
+firstRowSum = 9-5 -> 4
+
+minSum = min(minSum, max(4,1)) => min(10, 4) => 4
+SecondSum = 1+5 => 6
+
+
+
+i = 2
+firstRowSum = 4-4 -> 0
+
+minSum = min(minSum, max(0,6)) => min(4, 6) => 4
+SecondSum = 1+6 => 7
+
+
+ */
