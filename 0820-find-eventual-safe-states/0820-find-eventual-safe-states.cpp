de7f152c95e058dd5b78c0f95eaf46d0a587332a
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-01-24 11:27:36 +0530

    Time: 0 ms (100%), Space: 52.8 MB (75.14%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,5 +1,55 @@
 class Solution {
 public:
+    bool dfs(int node,vector<int> &visited,vector<int> &pathVisited,vector<int> &checked,vector<vector<int>> &graph)
+    {
+        visited[node]=1;
+        pathVisited[node]=1;
+
+        for(int nei : graph[node])
+        {
+            if(!visited[nei])
+            {
+                if(dfs(nei,visited,pathVisited,checked,graph))
+                {
+                    return true;
+                }
+            }
+              else if(pathVisited[nei]==1)
+                {
+                    return true;
+                }
+        }
+        checked[node]=1;
+        pathVisited[node]=0;
+        return false;
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+     int n = graph.size();
+     vector<int> visited(n,0);
+     vector<int> pathVisited(n,0);
+     vector<int> checked(n,0);
+     vector<int> safe;
+
+     for(int i=0;i<n;i++)
+     {
+        if(!visited[i])
+        {
+            dfs(i,visited,pathVisited,checked,graph);
+        }
+     }
+
+     for(int i=0;i<n;i++)
+     {
+        if(checked[i]==1)
+        safe.push_back(i);
+     }
+     return safe;
+    }
+};
+
+/*
+class Solution {
+public:
     int n;
     vector<int> degreein;
     vector< vector<int> > rGraph;
@@ -36,7 +86,7 @@ public:
         return ans;
     }
 };
-
+*/
 
 /*
 
