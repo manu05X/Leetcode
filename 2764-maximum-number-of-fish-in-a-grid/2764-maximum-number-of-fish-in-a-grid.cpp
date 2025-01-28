79986b7ba2fb908aab48730cc8650879946e1c47
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-01-28 09:32:39 +0530

    Time: 1 ms (88.62%), Space: 93.9 MB (94.22%) - LeetHub

--- 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
@@ -1,4 +1,43 @@
 class Solution {
+    public:
+    int dfs(vector<vector<int>>& grid, int n, int m, int i, int j){
+        if (i < 0 || i >= n || j < 0 || j >= m || grid[i][j] == 0) {
+            return 0;
+        }
+
+        int currFish = grid[i][j];
+        grid[i][j] = 0;
+
+        // Explore all 4 directions and accumulate the fish count
+        currFish += dfs(grid, n, m, i - 1, j); // Up
+        currFish += dfs(grid, n, m, i + 1, j); // Down
+        currFish += dfs(grid, n, m, i, j - 1); // Left
+        currFish += dfs(grid, n, m, i, j + 1); // Right
+
+        return currFish;
+
+    }
+
+    int findMaxFish(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int maxFish = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] != 0){
+                    maxFish = max(maxFish, dfs(grid, n, m, i,j));
+                }
+            }
+        }
+
+        return maxFish;
+    }
+};
+
+
+/*
+class Solution {
 public:
     int dfs(vector<vector<int>>& grid, int n, int m, int i, int j, vector<vector<bool>>& visited) {
         // Boundary check and conditions to stop DFS
@@ -43,7 +82,7 @@ public:
         return maxFish;
     }
 };
-
+*/
 
 /*
 
