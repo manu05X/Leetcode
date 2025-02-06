a69d6df1ffae758b48cca9663f10a5511586e265
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-02-06 07:46:28 +0530

    Time: 2 ms (98.9%), Space: 44.9 MB (61.48%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,3 +1,43 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        Map<Integer, Integer> mp = new HashMap<>();
+        int[] ans = new int[2];
+
+        for(int i = 0; i < n; i++){
+            int diff = target-nums[i];
+            if(mp.containsKey(diff) == true){
+                ans[0] = mp.get(diff);
+                ans[1] = i;
+
+                return ans;
+            }
+
+            mp.put(nums[i], i);
+        }
+
+        return ans;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //6.1.24
 // class Solution {
 //     public int[] twoSum(int[] nums, int target) {
@@ -47,26 +87,29 @@
 //     }
 // }
 
-// using entrySet of map
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
-        Map<Integer, Integer> mp=new HashMap<>();
-        //int ans[] = new int[2];
 
-        for(int i=0; i<n;i++)
-        {
-          int difference=target-nums[i]; //9-2=7
-          
-          // Using entrySet to iterate over the map
-            for (Map.Entry<Integer, Integer> entry : mp.entrySet()) {
-                if (entry.getKey() == difference) {
-                    return new int[] { entry.getValue(), i };
-                }
-            }
-          //      k     V
-          mp.put(nums[i],i);
-        }
-        return new int[]{};
-    }
-}
+
+// // using entrySet of map
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         int n = nums.length;
+//         Map<Integer, Integer> mp=new HashMap<>();
+//         //int ans[] = new int[2];
+
+//         for(int i=0; i<n;i++)
+//         {
+//           int difference=target-nums[i]; //9-2=7
+
+//           // Using entrySet to iterate over the map
+//             for (Map.Entry<Integer, Integer> entry : mp.entrySet()) {
+//                 if (entry.getKey() == difference) {
+//                     return new int[] { entry.getValue(), i };
+//                 }
+//             }
+//           //      k     V
+//           mp.put(nums[i],i);
+//         }
+//         return new int[]{};
+//     }
+// }
+
