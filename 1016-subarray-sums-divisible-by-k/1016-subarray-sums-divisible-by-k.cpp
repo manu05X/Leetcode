c8bcb5fe56ede18a66251c651ffc10cf83619c86
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-02-07 06:26:56 +0530

    Time: 1 ms (89.7%), Space: 34.4 MB (90.92%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
@@ -2,6 +2,40 @@ class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
         // Create a hash map to store the frequency of each prefix sum mod k.
+        vector<int> prefixSumCount(k,0);
+        int countSubarrays = 0; // Initialize the number of subarrays divisible by k.
+        int cumulativeSum = 0; // This will keep track of the cumulative sum of elements.
+
+        prefixSumCount[0]=1;
+
+        // Iterate over each number in the array.
+        for (int num : nums) {
+            // Add current number to cumulative sum and do mod by k.
+            // The double mod ensures that cumulativeSum is positive.
+            int x = cumulativeSum + num;
+            cumulativeSum = (x % k + k) % k;
+
+            // If a subarray has cumulativeSum mod k equals to some previous subarray's cumulativeSum mod k,
+            // then the subarray in between is divisible by k.
+            countSubarrays += prefixSumCount[cumulativeSum];
+            // now increase the count of cumulativeSum in map
+            prefixSumCount[cumulativeSum]++;
+
+            // The prefixSumCount[cumulativeSum]++ increases the count of the
+            // current prefix sum mod k, which will be used for future subarray checks.
+        }
+
+        // Return the total count of subarrays with sum divisible by k.
+        return countSubarrays;
+    }
+};
+
+
+/*
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        // Create a hash map to store the frequency of each prefix sum mod k.
         unordered_map<int, int> prefixSumCount {{0, 1}};
         int countSubarrays = 0; // Initialize the number of subarrays divisible by k.
         int cumulativeSum = 0; // This will keep track of the cumulative sum of elements.
@@ -27,3 +61,4 @@ public:
         return countSubarrays;
     }
 };
+*/
