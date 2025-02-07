6b8edf3648a0ed5309ec7e54899aea2a9af316b6
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-02-07 07:02:58 +0530

    Time: 82 ms (91.91%), Space: 71.8 MB (43.71%) - LeetHub

--- 0325-maximum-size-subarray-sum-equals-k/0325-maximum-size-subarray-sum-equals-k.java
+++ 0325-maximum-size-subarray-sum-equals-k/0325-maximum-size-subarray-sum-equals-k.java
@@ -10,14 +10,17 @@ class Solution {
         for(int i = 0; i < n; i++){
             prefixSum += nums[i];
 
+            // Check if all of the numbers seen so far sum to k.
             if(prefixSum == k){
                 maxLength = i+1;
             }
 
+            // If any subarray seen so far sums to k, then update the length of the longest_subarray. 
             if(mp.containsKey(prefixSum-k) == true){
                 maxLength = Math.max(maxLength, i-mp.get(prefixSum-k));
             }
 
+            // Only add the current prefix_sum index pair to the map if the prefix_sum is not already in the map.
             if(!mp.containsKey(prefixSum)){
                 mp.put(prefixSum, i);
             }
