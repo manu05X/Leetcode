4de45f8092c03808e480851a10ac39247a212d44
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-02-09 18:51:18 +0530

    Time: 11 ms (81.91%), Space: 304.7 MB (82.06%) - LeetHub

---
+++ 2677-cousins-in-binary-tree-ii/2677-cousins-in-binary-tree-ii.cpp
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+
+
+class Solution {
+public:
+    TreeNode* replaceValueInTree(TreeNode* root) {
+        if (root == nullptr) {
+            return root;
+        }
+
+        queue<TreeNode*> nodeQueue;
+        nodeQueue.push(root);
+        int currentLevelSum = root->val;
+
+        while (!nodeQueue.empty()) {
+            int levelSize = nodeQueue.size();
+            int nextLevelSum = 0;
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode* currentNode = nodeQueue.front();
+                nodeQueue.pop();
+                // Update node value to cousin sum.
+                currentNode->val = currentLevelSum - currentNode->val;
+
+                // Calculate sibling sum.
+                int siblingSum =
+                    (currentNode->left != nullptr ? currentNode->left->val
+                                                  : 0) +
+                    (currentNode->right != nullptr ? currentNode->right->val
+                                                   : 0);
+
+                if (currentNode->left != nullptr) {
+                    nextLevelSum +=
+                        currentNode->left->val;  // Accumulate next level sum.
+                    currentNode->left->val =
+                        siblingSum;  // Update left child's value.
+                    nodeQueue.push(
+                        currentNode->left);  // Add to queue for next level.
+                }
+                if (currentNode->right != nullptr) {
+                    nextLevelSum +=
+                        currentNode->right->val;  // Accumulate next level sum.
+                    currentNode->right->val =
+                        siblingSum;  // Update right child's value.
+                    nodeQueue.push(
+                        currentNode->right);  // Add to queue for next level.
+                }
+            }
+
+            currentLevelSum = nextLevelSum;  // Update current level sum
+                                             // for next iteration.
+        }
+        return root;
+    }
+};
