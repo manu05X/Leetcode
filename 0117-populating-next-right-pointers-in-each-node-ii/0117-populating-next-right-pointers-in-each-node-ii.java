3b009d12137a4bfe415f1d334e1749378f36dd84
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-02-10 15:52:58 +0530

    Time: 2 ms (34.88%), Space: 43.9 MB (98.21%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
@@ -46,12 +46,7 @@ class Solution {
                 // Pop a node from the front of the queue
                 Node node = Q.poll();
                 
-                // This check is important. We don't want to
-                // establish any wrong connections. The queue will
-                // contain nodes from 2 levels at most at any
-                // point in time. This check ensures we only 
-                // don't establish next pointers beyond the end
-                // of a level
+                // This check is important. We don't want to establish any wrong connections. The queue will contain nodes from 2 levels at most at any point in time. This check ensures we only don't establish next pointers beyond the end of a level
                 if (i < size - 1) {
                     node.next = Q.peek();
                 }
