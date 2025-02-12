945834d15714e2f7f5f96cb2a4b9229150d44238
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-02-12 19:30:00 +0530

    Time: 18 ms (87.51%), Space: 70.5 MB (47.55%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,3 +1,43 @@
+ class Solution {
+public:
+    bool canMakeBouquets(vector<int>& bloomDay, int days, int m, int k) {
+        int bouquets = 0, flowers = 0;
+        for (int bloom : bloomDay) {
+            if (bloom <= days) {
+                flowers++;
+                if (flowers == k) {
+                    bouquets++;
+                    flowers = 0;  // Reset flower count after forming a bouquet
+                }
+            } else {
+                flowers = 0;  // Reset if non-bloomed flower breaks adjacency
+            }
+            if (bouquets >= m) return true;  // Early exit
+        }
+        return false;
+    }
+    
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        int n = bloomDay.size();
+        if ((long long)m * k > n) return -1;  // Check if making bouquets is impossible
+
+        int left = *min_element(bloomDay.begin(), bloomDay.end());  // Min bloom day
+        int right = *max_element(bloomDay.begin(), bloomDay.end()); // Max bloom day
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (canMakeBouquets(bloomDay, mid, m, k)) {
+                right = mid;  // Try a smaller number of days
+            } else {
+                left = mid + 1;  // Increase days if bouquets can't be made
+            }
+        }
+        return left;
+    }
+};
+
+
+/*
 class Solution {
 public:
     int minDays(vector<int>& bloomDay, int m, int k) {
@@ -15,9 +55,11 @@ public:
 
             // Count possible bouquets for 'mid' days
             for (int j = 0; j < n; ++j) {
+                // If the flower is not bloomed
                 if (bloomDay[j] > mid) {
                     flowers = 0; // Reset if flower has not bloomed
                 } else {
+                    // If the flower is bloomed, add to the set. Else reset the count.
                     ++flowers; // Count consecutive bloomed flowers
                     if (flowers >= k) {
                         bouquets++; // Make a bouquet
@@ -38,29 +80,4 @@ public:
     }
 };
 
-
-// class Solution {
-// public:
-//         int minDays(vector<int>& A, int m, int k) {
-//         int n = A.size(), left = 1, right = 1e9;
-//         if ((m * k) > n) 
-//             return -1;
-//         while (left < right) {
-//             int mid = (left + right) / 2, flow = 0, bouq = 0;
-//             for (int j = 0; j < n; ++j) {
-//                 if (A[j] > mid) {
-//                     flow = 0;
-//                 } else if (++flow >= k) {
-//                     bouq++;
-//                     flow = 0;
-//                 }
-//             }
-//             if (bouq < m) {
-//                 left = mid + 1;
-//             } else {
-//                 right = mid;
-//             }
-//         }
-//         return left;
-//     }
-// };
+*/
