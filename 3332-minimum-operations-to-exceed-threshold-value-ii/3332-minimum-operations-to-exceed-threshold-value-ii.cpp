21c839e26572452b6c1e43afd09db85d57a5eca2
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-02-13 09:06:08 +0530

    Time: 92 ms (74.27%), Space: 89 MB (97.35%) - LeetHub

--- 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.cpp
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(false);  
+        cin.tie(NULL);
+        
         int n = nums.size();
 
         priority_queue<long long, vector<long long>, greater<long long>> pq(nums.begin(), nums.end());
