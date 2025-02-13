9c7e5a6565a2523e5ff4fa300f28ac7f22d9057b
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-02-13 14:49:59 +0530

    Time: 8 ms (55.68%), Space: 23.9 MB (56.04%) - LeetHub

--- 1782-smallest-string-with-a-given-numeric-value/1782-smallest-string-with-a-given-numeric-value.cpp
+++ 1782-smallest-string-with-a-given-numeric-value/1782-smallest-string-with-a-given-numeric-value.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     string getSmallestString(int n, int k) {
-        string result(n, 0);
+        string result(n, 0);  // Initialize a string of size n with all null characters
 
+        // Start filling the string from the rightmost position (n-1) to left (0)
         for(int pos = n-1; pos >= 0; pos--){
-            int add = min(k-pos, 26);
-
-            result[pos] = (char)(add + 'a'-1);
-            k -= add;
+            int add = min(k - pos, 26); // The max we can add at this position is 26
+            result[pos] = (char)(add + 'a' - 1); // Convert to corresponding character
+            k -= add; // Reduce k by the added value
         }
 
-        return result;
+        return result; // Return the lexicographically smallest string
     }
 };
