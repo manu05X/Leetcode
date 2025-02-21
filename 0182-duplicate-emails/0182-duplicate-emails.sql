b99da1465a1542ce0ad55c3847a7007a72049054
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-02-21 10:51:10 +0530

    Time: 685 ms (46.58%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,12 +1,22 @@
 # Write your MySQL query statement below
 
-SELECT Email FROM
-(
-    SELECT 
-        email, COUNT(email) as NUM
-    FROM 
-        PERSON
-    GROUP BY
-        Email
-) as statictic
-WHERE num > 1
+-- SELECT Email FROM
+-- (
+--     SELECT 
+--         email, COUNT(email) as NUM
+--     FROM 
+--         PERSON
+--     GROUP BY
+--         Email
+-- ) as statictic
+-- WHERE num > 1
+
+
+select 
+    Email
+from 
+    Person
+group by 
+    Email
+having 
+    count(Email) > 1;
