1386229e6396bfe1bfb53a25facd8795b446721d
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-02-24 23:45:36 +0530

    Time: 0 ms (100%), Space: 16.9 MB (95.93%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,3 +1,45 @@
+class Solution {
+public:
+    int countLessEqual(vector<vector<int>>& matrix, int mid, int n) {
+        int elementCount = 0;
+        int row = n - 1;  // Start from the bottom-left corner
+        int col = 0;
+
+        while (row >= 0 && col < n) {
+            if (matrix[row][col] <= mid) {
+                elementCount += row + 1;  // Count all elements in the current column up to 'row'
+                col++;
+            } else {
+                row--;  // Move up
+            }
+        }
+        return elementCount;
+    }
+
+    // Only ONE definition of kthSmallest should exist
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n = matrix.size();
+        int left = matrix[0][0];
+        int right = matrix[n - 1][n - 1];
+
+        // Binary search on the value range
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int elementCount = countLessEqual(matrix, mid, n);
+
+            if (elementCount < k) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+};
+
+
+
+/*
 class Solution { // 28 ms, faster than 83.06%
 public:
     int kthSmallest(vector<vector<int>> &matrix, int k) {
@@ -13,3 +55,5 @@ public:
         return maxHeap.top();
     }
 };
+
+*/
