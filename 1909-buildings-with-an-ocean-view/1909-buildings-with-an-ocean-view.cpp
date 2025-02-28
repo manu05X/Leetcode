ca7dc87bee5f5113ba12edcfabd1a137546a08ab
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-02-28 17:43:20 +0530

    Time: 1 ms (61.95%), Space: 73 MB (53.03%) - LeetHub

--- 1909-buildings-with-an-ocean-view/1909-buildings-with-an-ocean-view.cpp
+++ 1909-buildings-with-an-ocean-view/1909-buildings-with-an-ocean-view.cpp
@@ -1,29 +1,57 @@
 class Solution {
 public:
     vector<int> findBuildings(vector<int>& heights) {
-        int n = heights.size();
-
-        
-        stack<int> st;
-        for(int i = 0; i < n; i++){
-            while(!st.empty() && heights[st.top()] <= heights[i]){
-                st.pop();
+        int size = heights.size();
+        int curMax = 0;
+        vector<int> ans;
+        for (int i = size - 1; i >= 0; i--) {
+            if (curMax < heights[i]) {
+                ans.push_back(i);
+                
             }
-            st.push(i);
+            curMax = max(curMax, heights[i]);
         }
 
-        vector<int> ans(st.size(), 0);
-        int x = st.size()-1;
-
-        while(!st.empty()){
-            ans[x] = st.top();
-            st.pop();
-            x--;
-        }
+        reverse(ans.begin(), ans.end());
         return ans;
+
 
     }
 };
+
+/*
+from right to left 
+store
+increasing 
+*/
+
+
+// class Solution {
+// public:
+//     vector<int> findBuildings(vector<int>& heights) {
+//         int n = heights.size();
+
+        
+//         stack<int> st;
+//         for(int i = 0; i < n; i++){
+//             while(!st.empty() && heights[st.top()] <= heights[i]){
+//                 st.pop();
+//             }
+//             st.push(i);
+//         }
+
+//         vector<int> ans(st.size(), 0);
+//         int x = st.size()-1;
+
+//         while(!st.empty()){
+//             ans[x] = st.top();
+//             st.pop();
+//             x--;
+//         }
+//         return ans;
+
+//     }
+// };
 
 /*
 [4,2,3,1]
