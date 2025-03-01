297a6ac4330b03ad0edf353309f518d3983b24ac
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-03-01 10:38:08 +0530

    Time: 0 ms (100%), Space: 13.3 MB (23.58%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -2,14 +2,17 @@ class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
         int n = nums.size();
+        // Pointer to place non-zero elements
         int wrIdx = 0;
 
         for(int i = 0; i < n; i++){
+            // Step 1: Merge adjacent equal elements if they are non-zero
             if(i < n-1 && nums[i] == nums[i+1]){
                 nums[i] = nums[i]*2;
                 nums[i+1] = 0;
             }
-
+            
+            // Step 2: Shift non-zero elements to the front
             if(nums[i] != 0){
                 if(i != wrIdx){
                     swap(nums[i], nums[wrIdx]);
