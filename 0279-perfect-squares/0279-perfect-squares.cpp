9fba2588d3cd4ac4bd3f1c68e834761637658eb5
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-03-02 15:37:45 +0530

    Time: 37 ms (86.38%), Space: 13 MB (82.56%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,18 +1,36 @@
 class Solution {
 public:
     int numSquares(int n) {
-        static vector<int> counter = {0};
-        while (counter.size() <= n) {
-            int m = counter.size();
-            int curr = 1e9;
-            for (int i = 1; i * i <= m; ++i) {
-                curr = min(curr, counter[m - i * i] + 1);
+        vector<int> dp(n + 1, INT_MAX);
+        dp[0] = 0;  // Base case: 0 can be represented with 0 perfect squares.
+
+        // Fill dp table for each number from 1 to n
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j * j <= i; ++j) {
+                dp[i] = min(dp[i], dp[i - j * j] + 1);
             }
-            counter.push_back(curr);
         }
-        return counter[n];
+
+        return dp[n];
     }
 };
+
+
+// class Solution {
+// public:
+//     int numSquares(int n) {
+//         static vector<int> counter = {0};
+//         while (counter.size() <= n) {
+//             int m = counter.size();
+//             int curr = 1e9;
+//             for (int i = 1; i * i <= m; ++i) {
+//                 curr = min(curr, counter[m - i * i] + 1);
+//             }
+//             counter.push_back(curr);
+//         }
+//         return counter[n];
+//     }
+// };
 
 
 // class Solution {
