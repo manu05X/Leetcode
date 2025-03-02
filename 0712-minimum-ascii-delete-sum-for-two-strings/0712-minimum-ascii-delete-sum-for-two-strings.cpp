cafe60b58580b6b69bdfe30238f89bd685bd8a3e
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-03-02 07:51:18 +0530

    Time: 15 ms (77.86%), Space: 18.1 MB (71.16%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -8,25 +8,28 @@ public:
 
         // Fill in the base case values
         for(int i = 1; i <= n; i++){
-            compCost[i][0] = compCost[i-1][0] + s1[i-1];
+            compCost[i][0] = compCost[i-1][0] + s1[i-1];  // Correct initialization for s1
         }
 
         for(int j = 1; j <= m; j++){
-            compCost[0][j] = compCost[0][j-1] + s2[j-1];
+            compCost[0][j] = compCost[0][j-1] + s2[j-1];  // FIXED: Use s2[j-1] instead of s1[j-1]
         }
 
-        // Fill the remaining cells using the Bellman Equation
+        // Fill the remaining cells using DP
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
                 if(s1[i-1] == s2[j-1]){
-                    compCost[i][j] = compCost[i-1][j-1];
+                    compCost[i][j] = compCost[i-1][j-1];  // No cost if characters match
                 } else {
-                    compCost[i][j] = min(s1[i-1] + compCost[i-1][j], s2[j-1] + compCost[i][j-1]);
+                    compCost[i][j] = min(
+                        s1[i-1] + compCost[i-1][j],  // Delete from s1
+                        s2[j-1] + compCost[i][j-1]   // Delete from s2
+                    );
                 }
             }
         }
 
-        // Return the answer for entire input strings
+        // Return the answer for the entire input strings
         return compCost[n][m];
     }
 };
