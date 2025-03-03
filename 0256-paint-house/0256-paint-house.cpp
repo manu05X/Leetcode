11ba34dbdab17599f68083c4c44845b465487d8d
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-03-03 13:42:56 +0530

    Time: 1 ms (33.57%), Space: 14.3 MB (24.82%) - LeetHub

--- 0256-paint-house/0256-paint-house.cpp
+++ 0256-paint-house/0256-paint-house.cpp
@@ -15,6 +15,7 @@ public:
         dp[0][2] = costs[0][2];
 
         for(int i = 1; i < n; i++){
+            // we do not consider dp[i-1][0] when calculating dp[i][0], because of the constraint that no two adjacent houses can have the same color.
             dp[i][0] = costs[i][0] + min(dp[i-1][1], dp[i-1][2]);
             dp[i][1] = costs[i][1]+ min(dp[i-1][0], dp[i-1][2]);
             dp[i][2] = costs[i][2]+ min(dp[i-1][0], dp[i-1][1]);
