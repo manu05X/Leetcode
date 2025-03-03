a33c14d540b144fb89b48e88ec01604abd72a4a2
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-03-03 12:48:11 +0530

    Time: 0 ms (100%), Space: 13 MB (78.81%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
@@ -1,53 +1,34 @@
-// class Solution {
-// public:
-//     int mincostTickets(vector<int>& days, vector<int>& costs) {
-//         // here we need to initialize thr dp array for 365 days but it wold be too big so w can initialize only for last day of days array
-//         int lastDay = days[days.size() - 1];
-//         vector<int> dp(lastDay + 1, 0);
-        
-//         int i = 0;
-//         for (int day = 1; day <= lastDay; day++) {
-//             if (day < days[i]) {
-//                 dp[day] = dp[day - 1];
-//             } else {
-//                 i++;
-//                 dp[day] = min({dp[day - 1] + costs[0],
-//                                dp[max(0, day - 7)] + costs[1],
-//                                dp[max(0, day - 30)] + costs[2]});
-//             }
-//         }
-     
-//         return dp[lastDay];
-//     }
-// };
-
-
-
 class Solution {
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        int n = days.size();
-        vector<int> dp(n, 0);
+        // here we need to initialize thr dp array for 365 days but it wold be too big so w can initialize only for last day of days array
+        int lastDay = days[days.size() - 1];
         
-        for (int i = n - 1; i >= 0; --i) {
-            // 1-day pass cost
-            int opt1 = costs[0] + (i + 1 < n ? dp[i + 1] : 0);
-            
-            // 7-day pass cost
-            int after7 = days[i] + 6;
-            auto it7 = upper_bound(days.begin() + i, days.end(), after7);
-            int j7 = it7 - days.begin();
-            int opt2 = costs[1] + (j7 < n ? dp[j7] : 0);
-            
-            // 30-day pass cost
-            int after30 = days[i] + 29;
-            auto it30 = upper_bound(days.begin() + i, days.end(), after30);
-            int j30 = it30 - days.begin();
-            int opt3 = costs[2] + (j30 < n ? dp[j30] : 0);
-            
-            dp[i] = min({opt1, opt2, opt3});
+        // Initialize a DP array to store the minimum cost for each day up to `lastDay`.
+        // `dp[day]` will represent the minimum cost to travel up to and including `day`.
+        vector<int> dp(lastDay + 1, 0);
+        
+        // `i` is used to iterate through the `days` array.
+        int i = 0;
+        
+        // Iterate through each day from 1 to `lastDay`.
+        for (int day = 1; day <= lastDay; day++) {
+            // If the current day is not in the `days` array, the cost remains the same as the previous day.
+            if (day < days[i]) {
+                dp[day] = dp[day - 1];
+            } else {
+                // If the current day is in the `days` array, calculate the minimum cost:
+                // 1. Buy a 1-day pass: cost = dp[day - 1] + costs[0]
+                // 2. Buy a 7-day pass: cost = dp[max(0, day - 7)] + costs[1]
+                // 3. Buy a 30-day pass: cost = dp[max(0, day - 30)] + costs[2]
+                i++;
+                dp[day] = min({dp[day - 1] + costs[0],
+                               dp[max(0, day - 7)] + costs[1],
+                               dp[max(0, day - 30)] + costs[2]});
+            }
         }
-        
-        return dp[0];
+     
+        // Return the minimum cost to travel up to the last day.
+        return dp[lastDay];
     }
 };
