b5d1ebec4c61475fb51cd42b232da0e2dee46374
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-03-03 16:13:49 +0530

    Time: 19 ms (73.05%), Space: 37.6 MB (39.01%) - LeetHub

--- 1060-longest-repeating-substring/1060-longest-repeating-substring.cpp
+++ 1060-longest-repeating-substring/1060-longest-repeating-substring.cpp
@@ -2,6 +2,27 @@ class Solution {
 public:
     int longestRepeatingSubstring(string s) {
         int n = s.size();
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        int maxLen = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = i + 1; j <= n; j++) {
+                if (s[i - 1] == s[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    maxLen = max(maxLen, dp[i][j]);
+                }
+            }
+        }
+        return maxLen;
+    }
+};
+
+
+/*
+class Solution {
+public:
+    int longestRepeatingSubstring(string s) {
+        int n = s.size();
         int left = 1, right = n - 1, ans = 0;
         
         while (left <= right) {
@@ -27,3 +48,5 @@ private:
         return false;
     }
 };
+
+*/
