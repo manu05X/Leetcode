5c13b53eab4f620978429bd1fcd70d212e1f8843
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-03-03 16:41:41 +0530

    Time: 0 ms (100%), Space: 19.4 MB (73.33%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,5 +1,66 @@
 class Solution {
 public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<int> parent(n);
+        vector<int> rank(n, 1);
+
+        // Initialize each city as its own parent
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+        }
+
+        // Find function with path compression
+        auto find = [&](int x) {
+            while (parent[x] != x) {
+                parent[x] = parent[parent[x]]; // Path compression
+                x = parent[x];
+            }
+            return x;
+        };
+
+        // Union function with rank optimization
+        auto unite = [&](int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+
+            if (rootX != rootY) {
+                if (rank[rootX] > rank[rootY]) {
+                    parent[rootY] = rootX;
+                } else if (rank[rootX] < rank[rootY]) {
+                    parent[rootX] = rootY;
+                } else {
+                    parent[rootY] = rootX;
+                    rank[rootX]++;
+                }
+            }
+        };
+
+        // Perform union operations for connected cities
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (isConnected[i][j] == 1) {
+                    unite(i, j);
+                }
+            }
+        }
+
+        // Count the number of unique parents (provinces)
+        int provinceCount = 0;
+        for (int i = 0; i < n; ++i) {
+            if (parent[i] == i) { // If the city is its own parent, it's a root
+                provinceCount++;
+            }
+        }
+
+        return provinceCount;
+    }
+};
+
+
+/*
+class Solution {
+public:
     void dfs(int node, vector<vector<int>>& isConnected, vector<bool>& visit) {
         visit[node] = true;
         for(int i = 0; i < isConnected.size(); i++){
@@ -26,7 +87,7 @@ public:
 };
 
 
-
+*/
 
 
 
