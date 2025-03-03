ffb76607d1c764a7bf68e0911f61d625ea13ba5d
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-03-03 15:38:45 +0530

    Time: 40 ms (30.21%), Space: 24.3 MB (13.75%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -2,6 +2,32 @@ class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
         int n = points.size();
+        if (n == 1) {
+            return 1;
+        }
+        int result = 2;
+        for (int i = 0; i < n; i++) {
+            unordered_map<double, int> cnt;
+            for (int j = 0; j < n; j++) {
+                if (j != i) {
+                    cnt[atan2(points[j][1] - points[i][1],
+                              points[j][0] - points[i][0])]++;
+                }
+            }
+            for (auto [h, count] : cnt) {
+                result = max(result, count + 1);
+            }
+        }
+        return result;
+    }
+};
+
+
+/*
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& points) {
+        int n = points.size();
         if (n <= 2) return n; // If there are <=2 points, they are always on a line
 
         int maxCount = 1;
@@ -40,3 +66,4 @@ public:
         return maxCount;
     }
 };
+*/
