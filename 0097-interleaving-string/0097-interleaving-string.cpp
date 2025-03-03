e583b30f1afb53e0404cb9b67c4a625871849d64
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-03-03 13:49:13 +0530

    Time: 0 ms (100%), Space: 8.5 MB (84.96%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,6 +1,42 @@
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
+        int m = s1.size(), n = s2.size();
+        // If the lengths don't add up, s3 cannot be an interleaving.
+        if(m + n != s3.size()) return false;
+        
+        // dp[j] represents whether s1[0..i-1] and s2[0..j-1] can interleave to form s3[0..i+j-1].
+        vector<bool> dp(n + 1, false);
+        dp[0] = true;
+        
+        // Initialize the dp array for the case when s1 is empty.
+        for (int j = 1; j <= n; j++) {
+            dp[j] = dp[j - 1] && (s2[j - 1] == s3[j - 1]);
+        }
+        
+        // Iterate over s1.
+        for (int i = 1; i <= m; i++) {
+            // Update dp[0] for the case when s2 is empty.
+            dp[0] = dp[0] && (s1[i - 1] == s3[i - 1]);
+            
+            // Update dp[j] for j from 1 to n.
+            for (int j = 1; j <= n; j++) {
+                // Check if the current character of s1 can be used (and dp[j] was true from previous row)
+                // or if the current character of s2 can be used (and dp[j-1] was true in current row).
+                dp[j] = (dp[j] && (s1[i - 1] == s3[i + j - 1])) ||
+                        (dp[j - 1] && (s2[j - 1] == s3[i + j - 1]));
+            }
+        }
+        
+        return dp[n];
+    }
+};
+
+
+/*
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
         int n = s1.length();
         int m = s2.length();
         int l = s3.length();
@@ -35,3 +71,4 @@ public:
         return dp[n][m];
     }
 };
+*/
