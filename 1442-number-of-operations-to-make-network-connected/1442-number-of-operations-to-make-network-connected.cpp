c066bca1cef821885a0ead55e9282bd2e50106a0
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-03-03 17:53:05 +0530

    Time: 5 ms (80.83%), Space: 42 MB (93.59%) - LeetHub

---
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,50 @@
+class DSU {
+private:
+    vector<int> parent, rank;
+public:
+    DSU(int n) {
+        parent.resize(n);
+        rank.resize(n, 1);
+        for (int i = 0; i < n; ++i) parent[i] = i; // Each node is its own parent
+    }
+
+    int find(int x) {
+        if (parent[x] != x) parent[x] = find(parent[x]); // Path compression
+        return parent[x];
+    }
+
+    bool unite(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX == rootY) return false; // Already in the same component
+
+        // Union by rank
+        if (rank[rootX] > rank[rootY]) {
+            parent[rootY] = rootX;
+        } else if (rank[rootX] < rank[rootY]) {
+            parent[rootX] = rootY;
+        } else {
+            parent[rootY] = rootX;
+            rank[rootX]++;
+        }
+        return true;
+    }
+};
+
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        if (connections.size() < n - 1) return -1; // Not enough cables
+
+        DSU dsu(n);
+        int components = n;
+        
+        for (auto& conn : connections) {
+            if (dsu.unite(conn[0], conn[1])) {
+                components--; // Reduce number of separate components
+            }
+        }
+
+        return components - 1; // Minimum operations needed
+    }
+};
