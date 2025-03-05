1d7fc197b406844f5ddb65b1c1c45baedd2f2f3d
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-03-05 17:18:46 +0530

    Time: 0 ms (100%), Space: 13 MB (68.81%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,48 +1,54 @@
-class DSU {
-private:
+class DSU{
+public:
     vector<int> parent;
     vector<int> rank;
 
-public:
-    // Constructor to initialize DSU
-    DSU(int n) {
-        parent.resize(n + 1); // n+1 to accommodate 1-based indexing
-        rank.resize(n + 1, 1);
-        for (int i = 1; i <= n; ++i) {
-            parent[i] = i; // Each node is its own parent initially
+    DSU(int n){
+        rank.resize(n+1,1);
+        parent.resize(n+1);
+
+        for(int i = 0; i < n; i++){
+            parent[i] = i;
         }
     }
 
-    // Find function with path compression
-    int find(int x) {
-        if (parent[x] != x) {
-            parent[x] = find(parent[x]); // Path compression
+    int find(int x){
+        // if(parent[x] != x){
+        //     parent[x] = find(parent[x]);
+        // }
+        // return parent[x];
+
+        while(parent[x] != x){
+            parent[x] = parent[parent[x]];
+            x = parent[x];
         }
-        return parent[x];
+        return x;
     }
 
-    // Union function with rank optimization
-    bool unite(int x, int y) {
+    bool unionSet(int x, int y){
         int rootX = find(x);
         int rootY = find(y);
 
-        if (rootX == rootY) {
-            return false; // If both nodes have the same root, adding this edge creates a cycle
+        // If both nodes have the same root, adding this edge creates a cycle
+        if(rootX == rootY){
+            return false;
         }
 
-        // Union by rank
-        if (rank[rootX] > rank[rootY]) {
+        // Union by rank: attach the smaller tree under the root of the larger tree
+        if(rank[rootX] > rank[rootY]){
             parent[rootY] = rootX;
         } else if (rank[rootX] < rank[rootY]) {
             parent[rootX] = rootY;
         } else {
+            // If ranks are equal, attach 'y' to 'x' and increase rank of 'x'
             parent[rootY] = rootX;
             rank[rootX]++;
         }
-
         return true;
     }
+
 };
+
 
 class Solution {
 public:
@@ -50,59 +56,12 @@ public:
         int n = edges.size();
         DSU dsu(n);
 
-        for (auto& edge : edges) {
-            if (!dsu.unite(edge[0], edge[1])) {
-                return edge; // This edge forms a cycle, return it
-            }
-        }
-
-        return {}; // This should never be reached
-    }
-};
-
-
-
-
-/*
-class Solution {
-private:
-    // Performs DFS and returns true if there's a path between src and target.
-    bool isConnected(int src, int target, vector<bool>& visited,
-                     vector<int> adjList[]) {
-        visited[src] = true;
-
-        if (src == target) {
-            return true;
-        }
-
-        int isFound = false;
-        for (int adj : adjList[src]) {
-            if (!visited[adj]) {
-                isFound = isFound || isConnected(adj, target, visited, adjList);
-            }
-        }
-
-        return isFound;
-    }
-
-public:
-    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        int N = edges.size();
-
-        vector<int> adjList[N];
-        for (auto edge : edges) {
-            vector<bool> visited(N, false);
-
-            // If DFS returns true, we will return the edge.
-            if (isConnected(edge[0] - 1, edge[1] - 1, visited, adjList)) {
+        for(auto& edge : edges){
+            if(!dsu.unionSet(edge[0], edge[1])){
                 return edge;
             }
-
-            adjList[edge[0] - 1].push_back(edge[1] - 1);
-            adjList[edge[1] - 1].push_back(edge[0] - 1);
         }
 
         return {};
     }
 };
-*/
