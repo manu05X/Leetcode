7acb0fa1caafbe9e47ec939801d4a2b3be8a403e
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-03-05 21:17:11 +0530

    Time: 0 ms (100%), Space: 18 MB (76.61%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,5 +1,41 @@
 class Solution {
 public:
+    // DFS function to perform flood fill
+    void dfs(vector<vector<int>>& image, int i, int j, int originalColor, int newColor) {
+        int n = image.size();
+        int m = image[0].size();
+
+        // Base case: If the current cell is out of bounds or does not have the original color, return
+        if (i < 0 || i >= n || j < 0 || j >= m || image[i][j] != originalColor) {
+            return;
+        }
+
+        // Change the color of the current cell to the new color
+        image[i][j] = newColor;
+
+        // Recursively visit all four neighboring cells (up, down, left, right)
+        dfs(image, i - 1, j, originalColor, newColor); // up
+        dfs(image, i + 1, j, originalColor, newColor); // down
+        dfs(image, i, j - 1, originalColor, newColor); // left
+        dfs(image, i, j + 1, originalColor, newColor); // right
+    }
+
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        int originalColor = image[sr][sc]; // Get the original color of the starting pixel
+
+        // If the original color is the same as the new color, no need to perform flood fill
+        if (originalColor != newColor) {
+            dfs(image, sr, sc, originalColor, newColor);
+        }
+
+        return image;
+    }
+};
+
+
+/*
+class Solution {
+public:
     int dx[4] = {-1, 1, 0, 0};  // Direction arrays for 4 possible moves (up, down, left, right)
     int dy[4] = {0, 0, -1, 1};
 
@@ -40,44 +76,6 @@ public:
                 dfs(image, newX, newY, newColor, currColor, visited);  // Recur for the next cell
             }
         }
-    }
-};
-
-
-
-/*
-class Solution {
-public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        ios_base::sync_with_stdio(false);  
-        cin.tie(NULL);
-        
-        if(image[sr][sc] == newColor){
-            return image;
-        }
-        backtrackFlood(image,image[sr][sc],sr, sc, newColor);
-
-        return image;
-    }
-
-    void backtrackFlood(vector<vector<int>>& image, int color ,int sr, int sc, int newColor)
-    {
-        if(sc < 0 || sr < 0 || sr >= image.size() || sc >= image[0].size() || image[sr][sc] != color)
-        {
-            return;
-        }
-        image[sr][sc] = newColor;
-
-        int topRow = sr-1;
-        int bottomRow = sr+1;
-        int rightCol = sc + 1;
-        int leftCol = sc-1;
-
-        backtrackFlood(image, color, topRow, sc, newColor);
-        backtrackFlood(image, color, bottomRow, sc, newColor);
-        backtrackFlood(image, color, sr, rightCol, newColor);
-        backtrackFlood(image, color, sr, leftCol, newColor);
-
     }
 };
 */
