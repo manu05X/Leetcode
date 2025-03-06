7214fbc0e0188714b9073731cd94e586997ff001
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-03-06 09:50:40 +0530

    Time: 0 ms (100%), Space: 40.8 MB (10.6%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,17 +1,19 @@
 class Solution {
     public int fib(int n) {
-         if(n <= 1){
-            return n;
-        }
+        // Base cases
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        // Initialize variables for (n-2) and (n-1)
+        int prevStep = 0; // F(0)
+        int currStep = 1; // F(1)
         
-        int prevStep = 0;
-        int currStep = 1;
-        
+        // Iterate from 2 to n
         for(int i = 2; i <= n; i++){
-            int nextStep = currStep + prevStep;
+            int nextStep = currStep + prevStep; // F(i) = F(i-1) + F(i-2)
             
-            prevStep = currStep;
-            currStep = nextStep;
+            prevStep = currStep; // update the prevStep to currStep
+            currStep = nextStep; // update the currStep to nextStep
         }
         
         return currStep;
