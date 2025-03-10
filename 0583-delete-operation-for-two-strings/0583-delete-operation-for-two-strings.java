20341106382178ef7ed9a8b4a3dec4170005d7da
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-03-10 14:25:39 +0530

    Time: 7 ms (77.23%), Space: 44.9 MB (50.88%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -1,3 +1,30 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+
+        // Create a DP table where dp[i][j] represents the LCS of word1[0...i-1] and word2[0...j-1]
+        int[][] dp = new int[m + 1][n + 1];
+
+        // Fill the DP table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        int lcsLength = dp[m][n]; // Longest Common Subsequence (LCS)
+
+        // Minimum deletions = (total length of both strings) - (twice the LCS length)
+        return (m - lcsLength) + (n - lcsLength);
+    }
+}
+
+/*
 public class Solution {
     public int minDistance(String s1, String s2) {
         int m = s1.length();
@@ -25,7 +52,7 @@ public class Solution {
         return dp[m][n];
     }
 }
-
+*/
 
 /*
 public class Solution {
