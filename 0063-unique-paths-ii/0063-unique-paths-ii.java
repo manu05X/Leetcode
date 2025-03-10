1d89a4a7f9185395de953e79a063c6c225cfb016
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-03-10 10:41:07 +0530

    Time: 0 ms (100%), Space: 41.5 MB (96.09%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -8,7 +8,7 @@ class Solution {
             return 0;
         }
 
-        // Initialize the starting cell
+        // Initialize the starting cell, Number of ways of reaching the starting cell = 1.
         obstacleGrid[0][0] = 1;
 
         // Fill the first column, considering obstacles
@@ -30,6 +30,8 @@ class Solution {
         }
 
         // Fill the rest of the grid using dynamic programming
+        // Starting from obstacleGrid[1][1], we fill up the values.
+		// The number of ways of reaching obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1]
         for (int i = 1; i < n; i++) {
             for (int j = 1; j < m; j++) {
                 if (obstacleGrid[i][j] == 1) {
