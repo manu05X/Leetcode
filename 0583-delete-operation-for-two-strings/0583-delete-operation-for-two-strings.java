b360416f1dd200513b164bb105512d3a4cd5e185
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-03-10 14:21:23 +0530

    Time: 7 ms (77.23%), Space: 44.7 MB (83.68%) - LeetHub

---
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -0,0 +1,53 @@
+public class Solution {
+    public int minDistance(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+
+        // Find the longest common subsequence (LCS)
+        int lcsLength = lcs(s1, s2, m, n);
+        
+        // The minimum steps to make the two strings the same is (total length of both strings) - (twice the LCS length)
+        return (m - lcsLength) + (n - lcsLength);
+        // return s1.length() + s2.length() - 2 * lcs(s1, s2, s1.length(), s2.length());
+    }
+    public int lcs(String s1, String s2, int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
+
+
+/*
+public class Solution {
+    public int minDistance(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+
+        // Find the longest common subsequence (LCS)
+        int lcsLength = lcs(s1, s2, m, n);
+        
+        // The minimum steps to make the two strings the same is (total length of both strings) - (twice the LCS length)
+        return (m - lcsLength) + (n - lcsLength);
+        // return s1.length() + s2.length() - 2 * lcs(s1, s2, s1.length(), s2.length());
+    }
+    public int lcs(String s1, String s2, int m, int n) {
+        if (m == 0 || n == 0)
+            return 0;
+        if (s1.charAt(m - 1) == s2.charAt(n - 1))
+            return 1 + lcs(s1, s2, m - 1, n - 1);
+        else
+            return Math.max(lcs(s1, s2, m, n - 1), lcs(s1, s2, m - 1, n));
+    }
+}
+
+*/
