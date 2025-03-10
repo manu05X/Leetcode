dc23578188d6a80d5e98cf6e361f3c7c61d34a4c
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-03-10 19:34:52 +0530

    Time: 39 ms (60.28%), Space: 54.9 MB (82.43%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,4 +1,34 @@
 class Solution {
+    public int longestPalindromeSubseq(String s) {
+        // Initializing a lookup table of dimensions len(s) x len(s)
+        int lookupTable[][] = new int[s.length()][s.length()];
+
+        // Every sequence with one element is a palindrome of length 1
+        for (int i = 0; i< s.length(); i++)
+            lookupTable[i][i]=1;  
+             
+        for (int startIndex = s.length(); startIndex >= 0; startIndex--)
+            for (int endIndex = startIndex + 1; endIndex < s.length(); endIndex++)
+                
+                // case 1: elements at the beginning and the end are the same
+                if ( s.charAt(startIndex) == s.charAt(endIndex)){
+                    lookupTable[startIndex][endIndex] = 2 + lookupTable[startIndex + 1][endIndex - 1];
+                }
+                
+                // case 2: skip one element either from the beginning or the end
+                else{
+                    lookupTable[startIndex][endIndex]= Math.max(lookupTable[startIndex + 1][endIndex], lookupTable[startIndex][endIndex - 1]);
+                }
+        return lookupTable[0][s.length()-1];
+    }
+}
+
+
+
+
+
+/*
+class Solution {
     public static int longestPalindromicSubsequenceRecursive(int[][] lookupTable, String s, int startIndex, int endIndex) {
         if (startIndex > endIndex) 
             return 0;
@@ -29,6 +59,8 @@ class Solution {
         return longestPalindromicSubsequenceRecursive(lookupTable, s, 0, s.length() - 1);
     }
 }
+
+*/
 
 /*
 class Solution {
