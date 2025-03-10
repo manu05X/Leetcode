63237120301f26590492f13a2bec89e4b80476e9
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-03-10 09:55:11 +0530

    Time: 0 ms (100%), Space: 40.2 MB (91.43%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,4 +1,55 @@
 class Solution {
+    public int climbStairsRecur(int n, int[] lookupArr){
+
+        //Negative staircases i.e., invalid input
+        if(n < 0) 
+            return 0;
+        // if 0 staircase
+        else if(n == 0)
+        { 
+            return 1;
+        }
+        //If already present in the table
+        else if(lookupArr[n] > -1){
+            return lookupArr[n];
+        }
+        else   // Check each possible combination i.e The total number of ways that the child can hop the stairs
+        {
+            lookupArr[n] = climbStairsRecur(n-1, lookupArr) + climbStairsRecur(n-2, lookupArr);
+        }
+        return lookupArr[n];
+    }
+
+    public int climbStairs(int n) {
+        int[] lookupArr = new int[n+1];
+        for(int i = 0; i < n+1; i++){
+            lookupArr[i] = -1;
+        }
+
+        return climbStairsRecur(n, lookupArr);
+    }
+}
+
+
+/*
+class Solution {
+    public int climbStairs(int n) {
+        // Base Conditions
+        if(n < 0) return 0;
+        else if(n == 0)
+        { 
+            return 1;
+        }
+        else   // Check each possible combination i.e The total number of ways that the child can hop the stairs
+        {
+            return climbStairs(n-1) + climbStairs(n-2);
+        }
+    }
+}
+*/
+
+/*
+class Solution {
     public int climbStairs(int n) {
         // Base cases
         if(n == 1 || n == 0){
@@ -18,3 +69,4 @@ class Solution {
         return currStep;
     }
 }
+*/
