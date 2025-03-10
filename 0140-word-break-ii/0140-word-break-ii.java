b0631923ba27fcdc914ed14a0b14602cc056d863
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-03-10 18:45:00 +0530

    Time: 4 ms (52.85%), Space: 42.3 MB (10.44%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.java
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -1,4 +1,43 @@
 class Solution {
+    public List<String> wordBreak(String s, List<String> wordDict) {
+         // Initializing a table of size s.length + 1
+        ArrayList<ArrayList<String>> dpSolutions = new ArrayList<ArrayList<String>>();
+        for (int i = 0; i <= s.length(); i++)
+            dpSolutions.add(new ArrayList<String>());
+        
+        // Setting base case
+        dpSolutions.get(0).add("");
+
+        // For each substring in the input string, repeat the process.
+        for (int i = 1; i <= s.length(); i++) {
+            // Iterate over the current substring and break it down into all possible prefixes.
+            for (int j = 0; j < i; j++) {
+                String prefix = s.substring(j, i);
+
+                // Check if the current prefix exists in word_dict. If it does, we know that it is a valid word
+                // and can be used as part of the solution.
+                if (wordDict.contains(prefix)) {
+                    // Check if any part of the current substring already exists in the dp_solutions array.
+                    for (String substrings : dpSolutions.get(j)) {
+                        // Merge the prefix with the already calculated results
+                        dpSolutions.get(i).add((substrings + " " + prefix).trim());
+                    }
+                }
+            }
+        }
+
+        // Returning all the sentences formed using a complete string s.
+        return dpSolutions.get(s.length());
+    }
+}
+
+
+
+
+
+/*
+
+class Solution {
     // helper function that breaks down the string into words from substrings
     private static List<String> wordBreakRec(String s, List<String> dictionary) {
         // if s is an empty string
@@ -14,8 +53,8 @@ class Solution {
                 continue;
             if (word.length() == s.length())
                 res.add(s);
-            else {
-                // otherwise, break down the remaining part of s and append the current word to each result
+            else {// otherwise, break down the remaining part of s 
+                // and append the current word to each result
                 List<String> resultOfTheRest = wordBreakRec(s.substring(word.length()), dictionary);
                 for (String item : resultOfTheRest) {
                     item = word + " " + item;
@@ -31,3 +70,5 @@ class Solution {
         return wordBreakRec(s, wordDict);
     }
 }
+
+*/
