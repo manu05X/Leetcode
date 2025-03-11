9f93ce983c827d3ab2b5855f57f51c3cee8f5d75
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-03-11 17:36:54 +0530

    Time: 0 ms (100%), Space: 11.6 MB (36.64%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,6 +2,35 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
+        int lo = 0, mid = 0, hi = n-1;
+
+        while(mid <= hi){
+            switch(nums[mid]){
+                case 0:
+                    swap(nums[lo], nums[mid]);
+                    lo++;
+                    mid++;
+                    break;
+
+                case 1:
+                    mid++;
+                    break;
+
+                case 2:
+                    swap(nums[mid], nums[hi]);
+                    hi--;
+                    break;
+            }
+        }
+    }
+};
+
+/*
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
 
         int lo = 0, hi = n-1, mid=0;
 
@@ -20,3 +49,5 @@ public:
         }
     }
 };
+
+*/
