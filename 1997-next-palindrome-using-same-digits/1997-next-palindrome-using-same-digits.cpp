c4cfb6abccb0a62c1f0c6dae8f3ef652ef74a198
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-03-11 19:18:48 +0530

    Time: 0 ms (100%), Space: 13.3 MB (88%) - LeetHub

--- 1997-next-palindrome-using-same-digits/1997-next-palindrome-using-same-digits.cpp
+++ 1997-next-palindrome-using-same-digits/1997-next-palindrome-using-same-digits.cpp
@@ -1,6 +1,55 @@
 class Solution {
 public:
     string nextPalindrome(string num) {
+        int n = num.size() / 2;
+
+        if (n == 0) {
+            return "";
+        }
+
+        int right = n-1, pivot_idx = 0;
+
+        while (right > 0) {
+            if (num[right-1] < num[right]) {
+                pivot_idx = right-1;
+                break;
+            }
+            right--;
+        }
+
+        if (right == 0) {
+            return "";
+        }
+
+        int swap_idx = pivot_idx + 1;
+
+        for (int i = pivot_idx+1; i < n; i++) {
+            if (num[i] > num[pivot_idx] && num[i] < num[swap_idx]) {
+                swap_idx = i;
+            }
+        }
+
+        char temp = num[pivot_idx];
+        num[pivot_idx] = num[swap_idx];
+        num[swap_idx] = temp;
+
+        sort(num.begin() + pivot_idx+1, num.begin() + n);
+
+        int full_len = num.size();
+        for (int i = 0; i < n; i++) {
+            num[full_len-i-1] = num[i];
+        }
+
+        return num;
+    }
+};
+
+
+
+/*
+class Solution {
+public:
+    string nextPalindrome(string num) {
         int n = num.size();
         if (n <= 1) return "";  // Single-digit numbers cannot have a larger palindrome
 
@@ -42,3 +91,4 @@ public:
         return nextPal;
     }
 };
+*/
