a2d75253f8d7813516a875c35f50c754c845f721
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-03-11 18:36:48 +0530

    Time: 0 ms (100%), Space: 9.6 MB (30.43%) - LeetHub

--- 1356-minimum-number-of-moves-to-make-palindrome/1356-minimum-number-of-moves-to-make-palindrome.cpp
+++ 1356-minimum-number-of-moves-to-make-palindrome/1356-minimum-number-of-moves-to-make-palindrome.cpp
@@ -1,3 +1,33 @@
+// Greedy algo
+class Solution {
+public:
+    int minMovesToMakePalindrome(string s) {
+        if (s.empty()) return 0;  // Edge case: if string is empty, no moves needed.
+
+        int ans = 0;  // Stores the total number of swaps.
+
+        // Process the string until it becomes empty
+        while (s.size() > 0) {
+            const int i = s.find(s.back());  // Find the first occurrence of the last character.
+
+            if (i == s.size() - 1) {  
+                // If the character is unique (appears only once), it must be moved to the center.
+                ans += i / 2;  // Move the character towards the middle.
+            } else {
+                // If a matching pair is found earlier in the string
+                ans += i;  // Count swaps needed to move the character to the correct position.
+                s.erase(i, 1);  // Remove the matched character from the string.
+            }
+
+            s.pop_back();  // Remove the last character (since it has been matched/processed).
+        }
+        return ans;  // Return the total number of swaps.
+    }
+};
+
+
+
+/*
 class Solution {
 public:
     int minMovesToMakePalindrome(string s) {
@@ -31,3 +61,5 @@ public:
         return moves;  // Return the total number of moves required
     }
 };
+
+*/
