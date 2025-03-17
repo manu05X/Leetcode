da841ac87e2ccac1b6df3cd65fa831ccf52bc0c3
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-03-17 09:06:34 +0530

    Time: 0 ms (100%), Space: 32.2 MB (7.01%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,31 +1,59 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
+        // ios_base::sync_with_stdio(false);
+        // cin.tie(0);
+        // cout.tie(0);
+        
+        // int n = nums.size();
+        // int left = 0, total = 0;// Initialize start pointer to 0 and sum to 0
+        // int windowSize = n+1;// Initializing windowSize to a max number
+
+        // // Iterate over the input array
+        // for(int right = 0; right < n; right++)
+        // {
+        //     total += nums[right]; // Increase the sum by the current element
+        //     // check if we can remove elements from the start side of the subarray
+        //     // while still satisfying the target condition
+        //     while(total >= target)
+        //     {
+        //         // Finding size of current subarray
+        //         int currWindow = (right+1) - left;
+        //         // Update the answer with the new minimum length
+        //         if(currWindow < windowSize){
+        //             windowSize = currWindow;
+        //         }
+        //         // Subtract the first element of the subarray and move 'left' right
+        //         total -= nums[left];
+        //         left += 1;
+        //     }
+        // }
+
+        // if(windowSize != n+1)
+        //     return windowSize;
+        // else
+        //     return 0;
         int n = nums.size();
-        // Initialize start pointer to 0 and sum to 0
-        int start = 0, total = 0;
-        // Initializing windowSize to a max number
-        int windowSize = INT_MAX;
+        int left = 0, right = 0, total = 0;
+        int wnSize = n+1;
 
-        // Iterate over the input array
-        for(int i = 0; i < n; i++)
+        for(int i = 0; i<n; i++) 
         {
-            total += nums[i];
-            // check if we can remove elements from the start side of the subarray
-            // while still satisfying the target condition
-            while(total >= target)
-            {
-                // Finding size of current subarray
-                int currWindow = (i+1) - start;
-                windowSize = min(windowSize, currWindow);
-                total -= nums[start];
-                start += 1;
-            }
-        }
+           total+=nums[i];
+           while(total >= target) 
+           {
+            int curWindow = (i+1)-left;
+            if(curWindow < wnSize) 
+                wnSize = curWindow;
 
-        if(windowSize != INT_MAX)
-            return windowSize;
-        else
+            total-=nums[left];
+            left++;
+           }
+        }
+        
+        if(wnSize != n+1)
+            return wnSize;
+        else 
             return 0;
     }
 };
