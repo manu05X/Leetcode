44c9f76b9f6533236d83355dae455a0a25e4c1fb
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-03-26 07:59:48 +0530

    Time: 4 ms (70.77%), Space: 23.9 MB (59.56%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -12,7 +12,7 @@ public:
 
         int j = 0;// ans iterator
         for(int i = 1; i < n; i++)
-        {//first element of interval < than 2nd element of ans vector so merge
+        {//1st/start element of interval < than 2nd/end element of ans vector so merge
             if(intervals[i][0] <= ans[j][1])
             {// change the ans 2nd element by comparing interval's 2nd and ans 2nd element
                 ans[j][1] = max(intervals[i][1],ans[j][1]);
@@ -27,3 +27,15 @@ public:
         return ans;
     }
 };
+
+/*
+Algorithm:
+
+Sort the intervals based on the start times using a custom comparator.
+Initialize an empty list (ans) to store the merged intervals.
+Add the first interval to the ans list.
+Iterate through the sorted intervals starting from the second interval.
+    If the start time of the current interval is less than or equal to the end time of the last interval in ans, merge the intervals by updating the end time of the last interval in ans.
+    If they don't overlap, add the current interval to ans.
+Convert the list ans to a 2D array and return it.
+*/
