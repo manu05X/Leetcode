b6259230d748c79ac0fa2bbb0bd77db8b8fa095a
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-03-31 10:55:23 +0530

    Time: 55 ms (90.1%), Space: 58.8 MB (51%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -10,6 +10,41 @@ public:
         if (totalSum % 2 != 0) return false;
         int subSetSum = totalSum / 2;
         int n = nums.size();
+        vector<vector<optional<bool>>> memo(n + 1, vector<optional<bool>>(subSetSum + 1, nullopt));
+        return dfs(nums, n - 1, subSetSum, memo);
+    }
+
+    bool dfs(vector<int> &nums, int n, int subSetSum, vector<vector<optional<bool>>> &memo) {
+        // Base Cases
+        if (subSetSum == 0)
+            return true;
+        if (n == 0 || subSetSum < 0)
+            return false;
+        // check if subSetSum for given n is already computed and stored in memo
+        if (memo[n][subSetSum] != nullopt) {
+            return (memo[n][subSetSum] == true);
+        }
+        bool result = dfs(nums, n - 1, subSetSum - nums[n - 1], memo) ||
+                dfs(nums, n - 1, subSetSum, memo);
+        memo[n][subSetSum] = result;
+        return result;
+    }
+};
+
+
+/*
+class Solution {
+public:
+    bool canPartition(vector<int> &nums) {
+        int totalSum = 0;
+        // find sum of all array elements
+        for (int num : nums) {
+            totalSum += num;
+        }
+        // if totalSum is odd, it cannot be partitioned into equal sum subset
+        if (totalSum % 2 != 0) return false;
+        int subSetSum = totalSum / 2;
+        int n = nums.size();
         bool dp[n + 1][subSetSum + 1];
         memset(dp, 0, (n + 1) * (subSetSum + 1) * sizeof(bool));
         dp[0][0] = true;
@@ -25,3 +60,4 @@ public:
         return dp[n][subSetSum];
     }
 };
+*/
