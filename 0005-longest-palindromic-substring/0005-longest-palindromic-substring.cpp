f24b3d569236f8bdb490377c831cac423fb850f2
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-04-04 22:19:49 +0530

    Time: 4 ms (94.92%), Space: 11.4 MB (67.06%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,5 +1,57 @@
 class Solution {
 public:
+    string longestPalindrome(string s) {
+        string s_prime = "#";
+        for (char c : s) {
+            s_prime += c;
+            s_prime += "#";
+        }
+
+        int n = s_prime.length();
+        vector<int> palindrome_radii(n, 0);
+        int center = 0;
+        int radius = 0;
+
+        for (int i = 0; i < n; i++) {
+            int mirror = 2 * center - i;
+
+            if (i < radius) {
+                palindrome_radii[i] = min(radius - i, palindrome_radii[mirror]);
+            }
+
+            while (i + 1 + palindrome_radii[i] < n &&
+                   i - 1 - palindrome_radii[i] >= 0 &&
+                   s_prime[i + 1 + palindrome_radii[i]] ==
+                       s_prime[i - 1 - palindrome_radii[i]]) {
+                palindrome_radii[i]++;
+            }
+
+            if (i + palindrome_radii[i] > radius) {
+                center = i;
+                radius = i + palindrome_radii[i];
+            }
+        }
+
+        int max_length = 0;
+        int center_index = 0;
+        for (int i = 0; i < n; i++) {
+            if (palindrome_radii[i] > max_length) {
+                max_length = palindrome_radii[i];
+                center_index = i;
+            }
+        }
+
+        int start_index = (center_index - max_length) / 2;
+        string longest_palindrome = s.substr(start_index, max_length);
+
+        return longest_palindrome;
+    }
+};
+
+
+/*
+class Solution {
+public:
 
     int isPalindrome(string& s, int left, int right){
         while(left >= 0 && right < s.size() && s[left] == s[right]){
@@ -13,8 +65,7 @@ public:
         if (s.empty())
 		    return "";
 
-        // Initializing start and end indexes of resultant substring
-	// as zero
+        // Initializing start and end indexes of resultant substring as zero
 	    int start = 0, end = 0;
 
         for(int i = 0; i < n; i++){
@@ -23,7 +74,7 @@ public:
 
             //Now we have both half length len1 and len2 so we now calculate the max length of both
             int len = max(len1,len2);
-
+            
             if(len > end-start){
                 // Updating start and end indexes of palindromic substring
 			start = i - (len - 1) / 2;
@@ -34,3 +85,4 @@ public:
         return s.substr(start, end+1-start);
     }
 };
+*/
