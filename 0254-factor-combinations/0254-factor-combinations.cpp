d8f9722cddd37fd201176fbcf6cb5db963a64988
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-04-08 12:13:40 +0530

    Time: 612 ms (17.97%), Space: 12.3 MB (93.55%) - LeetHub

---
+++ 0254-factor-combinations/0254-factor-combinations.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void dfs(int start, int n, vector<int>& path, vector<vector<int>>& ans) {
+        if (n == 1) {
+            if (path.size() > 1) { // Only add if more than one factor
+                ans.push_back(path);
+            }
+            return;
+        }
+
+        // Loop from current number to n
+        for (int i = start; i <= n; i++) {
+            if(n%i == 0){
+                path.push_back(i);         // Choose number
+                dfs(i , n/i, path, ans); // allow reuse of same factor
+                path.pop_back();           // Backtrack
+            }
+        }
+    }
+
+    vector<vector<int>> getFactors(int n) {
+        if(n == 1){
+            return {};
+        }
+
+        vector<vector<int>> ans;
+        vector<int> path;
+        dfs(2, n, path, ans);
+        return ans;
+    }
+};
