eccecf437624cbd8870a92084b0f82daa9242bc6
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-04-08 08:44:13 +0530

    Time: 0 ms (100%), Space: 11.4 MB (81.6%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -26,3 +26,41 @@ public:
     }
 };
 
+
+
+
+/*
+class Solution {
+public:
+    void dfs(vector<int>& nums, vector<vector<int>>& ans, int idx){
+        if(idx == nums.size()){
+            ans.push_back(nums);
+            return ;
+        }
+
+        unordered_set<int> used; // to track used numbers at this level
+       
+
+        for(int i = idx; i < nums.size(); i++){
+            // if(i == idx || nums[i] != nums[i-1]){
+            if (used.count(nums[i])) continue; // skip duplicate at this level
+            used.insert(nums[i]);
+
+                swap(nums[idx], nums[i]);
+                dfs(nums, ans, idx+1);
+                swap(nums[idx], nums[i]);
+            // }
+        }
+        //sort(nums.begin()+idx, nums.end());
+    }
+
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+
+        dfs(nums, ans, 0);
+
+        return ans;
+    }
+};
+*/
