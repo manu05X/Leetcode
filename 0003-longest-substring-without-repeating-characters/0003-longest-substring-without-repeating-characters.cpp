4cbab18e13ec21f9abc1e19f4ae40a1de33a9689
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-04-23 15:30:41 +0530

    Time: 7 ms (68.59%), Space: 11.6 MB (78.34%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -3,28 +3,18 @@ public:
     int lengthOfLongestSubstring(string s) {
         int n = s.size();
         int ans = 0;
-        //map for keeping currChar index or last occurance of it
-        unordered_map<char, int> mp;
-        //last index of unique charter window
-        int left = 0; 
-
-        for(int right = 0; right < n; right++){
-            int currChar = s[right]; // current Character
-
-            //Checking the map if it contains the currChar
-            if(mp.find(currChar) != mp.end())
-            {
-                //updating left index as previous range has repeting char
-                left = max(mp[currChar],left); 
+        unordered_map<char,int> mp;
+        int left = 0;
+        for(int right = 0; right<n;right++) {
+            int curChar = s[right];
+            if(mp.find(curChar)!=mp.end()) {
+                left = max(mp[curChar], left);
             }
-            //current range of unique charter
-            int currDiff = (right - left +1);
-            ans = max(currDiff, ans);
-            //adding the currChar in map
-            mp[currChar] = right+1;
+            int curDiff = right - left +1;
+            ans = max(ans, curDiff);
+            mp[curChar] = right+1;
         }
-
-        return ans;
+     return ans;
     }
 };
 
@@ -40,7 +30,7 @@ l = 0, r = 2, currDist = 3, ans = 3, mp{a:1, b:2, c:3}
 l = 1, r = 3, currDist = 3, ans = 3, mp{a:4, b:2, c:3}, as a is repeting so change the left to where it was previously found i.e 1
 l = 2, r = 4, currDist = 3, ans = 3, mp{a:4, b:5, c:3} similarly for b=5, l = 2
 l = 3, r = 5, currDist = 3, ans = 3, mp{a:4, b:5, c:6} similarly for c=6, l = 3
-l = 5, r = 6, currDist = 2, ans = 3, mp{a:4, b:6, c:6} similarly for b=6, l = 5
+l = 5, r = 6, currDist = 2, ans = 3, mp{a:4, b:7, c:6} similarly for b=6, l = 5
 l = 7, r = 7, currDist = 1, ans = 3, mp{a:4, b:7, c:6} similarly for b=6, l = 7
 
 */
