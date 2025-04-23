688e00bb6a22ea05b64c4986ff9559b88fc4afa4
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-04-23 08:20:15 +0530

    Time: 4 ms (16.22%), Space: 22.1 MB (61.48%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        stack<int> st; // Stack to keep track of remaining asteroids after collisions
+        int n = asteroids.size();
+        stack<int> st;
 
-        for(int asteroid : asteroids){
-            bool flag = true; // Flag to track if the asteroid survives
+        for(int i = 0; i < n; i++){
+            int curr = asteroids[i];
+            bool explode = false;
 
-            // Check if a collision happens (Right-moving asteroid vs Left-moving asteroid)
-            while(!st.empty() && (st.top() > 0 && asteroid < 0)){
-                // If the top asteroid in the stack is smaller, then it explodes.
-                // Continue checking the next asteroid in the stack.
-                if(abs(st.top()) < abs(asteroid)){
+            while(!st.empty() && (curr < 0 && st.top() > 0)){
+                if(abs(st.top()) < abs(curr)){
                     st.pop();
                     continue;
-                } 
-                // If both asteroids are of the same size, both will explode.
-                else if(abs(st.top()) == abs(asteroid)){
+                } else if(abs(st.top()) == abs(curr)){
                     st.pop();
                 }
 
-                // If we reach here, the current asteroid is destroyed (either by equal or larger asteroid)
-                // So, we should not add it to the stack.
-                flag = false;
+                explode = true;
                 break;
             }
 
-            // If asteroid survives, push it onto the stack.
-            if(flag){
-                st.push(asteroid);
+            if(!explode){
+                st.push(curr);
             }
         }
 
-        // Transfer stack elements to result vector in reverse order.
         vector<int> ans(st.size());
         for (int i = ans.size() - 1; i >= 0; i--) {
             ans[i] = st.top();
