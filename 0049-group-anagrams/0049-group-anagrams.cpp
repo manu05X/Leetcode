54a0f110e0bb31e6da164c340b8127a84abbf544
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-05-13 14:54:11 +0530

    Time: 19 ms (49.19%), Space: 25.8 MB (61.06%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,27 +1,46 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
+        // ios_base::sync_with_stdio(false);
+        // cin.tie(NULL);
+        // cout.tie(NULL);
 
-        //map of string and vector<string>
-        unordered_map<string, vector<string> > m;
+        // //map of string and vector<string>
+        // unordered_map<string, vector<string> > m;
+        // vector<vector<string>> ans;
+
+        // // add string to map
+        // for(auto str : strs){
+        //     string s = str; // eat
+        //     // sort the string i.e s
+        //     sort(s.begin(), s.end());//key
+        //     //push_back map with key s and value from str
+        //     m[s].push_back(str);//value
+        // }
+        // // key -> value
+
+        // for(auto x : m){
+        //     ans.push_back(x.second);
+        // }
+
+        // return ans;
+        unordered_map<string, vector<string>> mp;
         vector<vector<string>> ans;
-
-        // add string to map
-        for(auto str : strs){
-            string s = str;
-            // sort the string i.e s
+        for(int i = 0;i< strs.size();i++) {
+            string s = strs[i];
             sort(s.begin(), s.end());
-            //push_back map with key s and value from str
-            m[s].push_back(str);
+            mp[s].push_back(strs[i]);
         }
-
-        for(auto x : m){
-            ans.push_back(x.second);
+        for(auto m : mp) {
+            ans.push_back(m.second);
         }
-
         return ans;
     }
 };
+
+/*
+key --> value
+["abt"] = {"bat"}, 
+["ant"] = {"tan", "nat"}, 
+["aet"] = {"eat", "tea", "ate"}}
+*/
