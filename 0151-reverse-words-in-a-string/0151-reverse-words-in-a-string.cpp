9e5d972252dfa9e27c8a46c5781ab588613563fa
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-05-16 12:31:29 +0530

    Time: 0 ms (100%), Space: 10 MB (82.44%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     string reverseWords(string s) {
+        reverse(s.begin(),s.end());
+        int n=s.size();
+        int left=0;
+        int right=0;
+        int i=0;
+        while(i<n){
+            while(i<n && s[i]==' ')i++;
+            if(i==n)break; // to stop index going out of bounds
+            while(i<n && s[i]!=' '){
+                s[right++]=s[i++];
+            }
+            reverse(s.begin()+left,s.begin()+right);
+            s[right++]=' ';
+            left=right;
+            i++;
+        }
+        s.resize(right-1);
+        return s;
+    }
+};
+
+/*
+class Solution {
+public:
+    string reverseWords(string s) {
         // Step 1: Remove leading, trailing, and extra spaces
         int n = s.size();
         string temp;
@@ -44,3 +69,5 @@ public:
         return s;
     }
 };
+
+*/
