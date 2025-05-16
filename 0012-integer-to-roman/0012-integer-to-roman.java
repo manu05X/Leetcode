a69e76d4507dca05c86e8a2d521b57f9f015fb74
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-05-16 23:30:47 +0530

    Time: 3 ms (98.53%), Space: 44.9 MB (17.15%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,51 +1,24 @@
-/*
-class Solution {
-    public String intToRoman(int num) {
-        int[] base = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String[] chars = new String[]{"M","CM","D","CD","C","XC","L","XL","X","IX", "V", "IV", "I"};
-        //expand the "base" char to make sure that the roman numeric can be only the 1 - 3 appearance of the characters above
-        String result = "";
-        for (int i = 0; i < base.length; i ++) {
-            if (num / base[i] > 0) {
-                for (int j = 0; j < num/base[i]; j ++) {
-                    result = result + chars[i];
-                }
-                num = num - (num / base[i]) * base[i];
-            }
-        }
-        return result;
-    }
-}
-*/
+import java.util.*;
 
 class Solution {
     public String intToRoman(int num) {
-        // Map to store integer values and their corresponding Roman numeral symbols
-        Map<Integer, String> romanMap = new LinkedHashMap<>();
-        romanMap.put(1000, "M");
-        romanMap.put(900, "CM");
-        romanMap.put(500, "D");
-        romanMap.put(400, "CD");
-        romanMap.put(100, "C");
-        romanMap.put(90, "XC");
-        romanMap.put(50, "L");
-        romanMap.put(40, "XL");
-        romanMap.put(10, "X");
-        romanMap.put(9, "IX");
-        romanMap.put(5, "V");
-        romanMap.put(4, "IV");
-        romanMap.put(1, "I");
+        // Array of Roman numeral values and their corresponding symbols
+        int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 
-        StringBuilder result = new StringBuilder(); // To build the resulting Roman numeral string
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
 
-        // Iterate through the map and construct the Roman numeral
-        for (Map.Entry<Integer, String> entry : romanMap.entrySet()) {
-            while (num >= entry.getKey()) { // While the number is greater than or equal to the key
-                result.append(entry.getValue()); // Append the corresponding Roman numeral
-                num -= entry.getKey(); // Decrease the number by the key value
+        StringBuilder result = new StringBuilder();
+
+        // Iterate through values and symbols
+        for (int i = 0; i < values.length && num > 0; i++) 
+        {
+            while (num >= values[i]) 
+            {
+                result.append(symbols[i]);  // Append corresponding symbol
+                num -= values[i];           // Subtract the value from number
             }
         }
 
-        return result.toString(); // Return the final Roman numeral string
+        return result.toString();  // Return the final Roman numeral string
     }
 }
