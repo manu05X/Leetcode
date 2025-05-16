4eb9c148f0d58d093e2685ac5fd54749d52aea8c
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-05-16 12:39:24 +0530

    Time: 0 ms (100%), Space: 9.8 MB (94.15%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,26 +1,48 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        reverse(s.begin(),s.end());
-        int n=s.size();
-        int left=0;
-        int right=0;
-        int i=0;
-        while(i<n){
-            while(i<n && s[i]==' ')i++;
-            if(i==n)break; // to stop index going out of bounds
-            while(i<n && s[i]!=' '){
-                s[right++]=s[i++];
+        // Step 1: Reverse the whole string to bring words in reverse order
+        reverse(s.begin(), s.end());
+
+        int n = s.size();
+        int left = 0;     // Start index of the current word
+        int right = 0;    // End index where the next word will be written
+        int i = 0;        // Pointer to traverse the original string
+
+        while (i < n) {
+            // Skip spaces
+            while (i < n && s[i] == ' ') i++;
+
+            // If we reach the end after skipping spaces, break
+            if (i == n) break;
+
+            // Start writing the word from 'right' index
+            // Copy the word (non-space characters) to the 'right' position
+            while (i < n && s[i] != ' ') {
+                s[right++] = s[i++];
             }
-            reverse(s.begin()+left,s.begin()+right);
-            s[right++]=' ';
-            left=right;
+
+            // Reverse the current word to restore correct order (since the whole string was reversed earlier)
+            reverse(s.begin() + left, s.begin() + right);
+
+            // Add a single space after the word
+            s[right++] = ' ';
+
+            // Set 'left' to the current 'right' for the next word
+            left = right;
+
+            // Move past the space
             i++;
         }
-        s.resize(right-1);
+
+        // Remove the trailing space by resizing the string
+        // If there were words, we remove the extra space added after the last word
+        s.resize(right > 0 ? right - 1 : 0);
+
         return s;
     }
 };
+
 
 /*
 class Solution {
