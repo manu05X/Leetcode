af6f6bdc1c810bd5fd9affdce5dadb9ffc9b4a0d
Manish Kumar <36466808+manu05X@users.noreply.github.com>
2025-05-21 10:56:02 +0530

    Time: 0 ms (100%), Space: 22.6 MB (78.03%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -17,32 +17,3 @@ public:
         return wd+1;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-// class Solution {
-// public:
-//     int removeDuplicates(vector<int>& nums) {
-//         int i = 0; 
-//         int j = 1;
-
-//         while(j < nums.size())
-//         {
-//             if(nums[i] != nums[j]){
-//                 i++;
-//                 nums[i] = nums[j];
-//             }
-//             j++;
-//         }
-//         return i+1;
-//     }
-// };
